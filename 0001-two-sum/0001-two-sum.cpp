5ddde3f7459bb111ddda66ed07001a6a78f05162
Li-Wei Yang <68542467+liver121888@users.noreply.github.com>
2023-07-03 14:33:49 +0800

    Time: 15 ms (65.15%), Space: 12.5 MB (5.56%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,17 +2,24 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         int length = nums.size();
-        int tmp, rest;
-        vector<int> ans;
+        int tmp {}, rest {};
+        vector<int> ans {};
+        
+        map<int, int> aMap;
+        
+        // we could use vector.at() or indexing, at() method would do 
+        // boundary check while indexing would not. Here we use indexing 
+        // because it's faster and 
         for (int i = 0; i < length; i++) {
-            tmp = nums.at(i);
-            rest = target - tmp;
-            for (int j = i + 1; j < length; j++) {
-                if (nums.at(j) == rest) {
-                    ans.push_back(i);
-                    ans.push_back(j);
-                    return ans;
-                }
+            aMap[nums[i]] = i;            
+        }
+        
+        for (int i = 0; i < length; i++) {
+            rest = target - nums.at(i);
+            if (aMap.find(rest) != aMap.end() && aMap[rest] != i) {
+                ans.push_back(i);
+                ans.push_back(aMap[rest]);
+                return ans;
             }
         }
         return ans;
