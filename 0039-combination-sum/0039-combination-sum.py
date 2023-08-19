b3cf744ff6612cce24c5bc560742f640811f2f8f
Li-Wei Yang <68542467+liver121888@users.noreply.github.com>
2023-08-19 10:11:31 +0800

    Time: 45 ms (99.08%), Space: 16.3 MB (83.30%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,5 +1,6 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        # discussion referred
         candidates.sort()
         n = len(candidates)
         ans = []
