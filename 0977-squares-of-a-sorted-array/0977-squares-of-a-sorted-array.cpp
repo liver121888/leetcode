6da17affd05c918fd780039b4ccff0b9418f6b69
Li-Wei Yang <68542467+liver121888@users.noreply.github.com>
2023-12-10 22:45:42 -0500

    Time: 24 ms (63.18%), Space: 26.5 MB (35.50%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -6,10 +6,13 @@ public:
         int r = n - 1;
         int l = 0;
         for (int i = n - 1; i >=0; i--) {
-            if (abs(nums[l]) > abs(nums[r]))
-                ans[i] = nums[l]*nums[l++];
-            else
-                ans[i] = nums[r]*nums[r--]; 
+            if (abs(nums[l]) > abs(nums[r])) {
+                ans[i] = nums[l]*nums[l];
+                l += 1; 
+            } else {
+                ans[i] = nums[r]*nums[r]; 
+                r -= 1;                
+            }
         }        
         return ans;        
     }
