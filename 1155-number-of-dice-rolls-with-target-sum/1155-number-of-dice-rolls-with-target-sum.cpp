2e5cffcedfab5ea827c47c388ab33feda5aceb48
Li-Wei Yang <68542467+liver121888@users.noreply.github.com>
2023-12-25 23:22:19 -0500

    Time: 45 ms (19.42%), Space: 6.8 MB (85.70%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -3,14 +3,17 @@ public:
     
     int numRollsToTarget(int n, int k, int target) {
         vector<int> dp = vector<int>(target + 1);
-        
+        vector<int> nextDp = vector<int>(target + 1);
+
         int MOD = 1e9 + 7;
         
         // base case
         dp[0] = 1;
 
         for (int dice = 0; dice < n; dice++) {
-            vector<int> nextDp = vector<int>(target + 1);
+            // vector<int> nextDp = vector<int>(target + 1);
+            fill(nextDp.begin(), nextDp.end(), 0);
+
             for (int val = 1; val < k + 1; val++) {
                 for (int total = val; total < target + 1; total++) {
                     nextDp[total] = (nextDp[total] + dp[total - val]) % MOD;
