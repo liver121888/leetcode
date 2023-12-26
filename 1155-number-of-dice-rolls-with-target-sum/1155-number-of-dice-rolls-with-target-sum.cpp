a84134959681ab45bab092b7f11261046e1bc457
Li-Wei Yang <68542467+liver121888@users.noreply.github.com>
2023-12-25 23:21:48 -0500

    Time: 43 ms (21.32%), Space: 8.6 MB (76.28%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -1,39 +1,24 @@
-#include <vector>
-#include <unordered_map>
-
 class Solution {
 public:
-    const int MOD = 1e9 + 7;
+    
+    int numRollsToTarget(int n, int k, int target) {
+        vector<int> dp = vector<int>(target + 1);
+        
+        int MOD = 1e9 + 7;
+        
+        // base case
+        dp[0] = 1;
 
-    int numRollsToTarget(int d, int f, int target) {
-        // Memoization table
-        std::unordered_map<std::string, int> memo;
-        return helper(d, f, target, memo);
-    }
-
-private:
-    int helper(int d, int f, int target, std::unordered_map<std::string, int>& memo) {
-        // Base cases
-        if (d == 0 && target == 0) {
-            return 1;
+        for (int dice = 0; dice < n; dice++) {
+            vector<int> nextDp = vector<int>(target + 1);
+            for (int val = 1; val < k + 1; val++) {
+                for (int total = val; total < target + 1; total++) {
+                    nextDp[total] = (nextDp[total] + dp[total - val]) % MOD;
+                }
+            }
+            dp = nextDp;
         }
-        if (d == 0 || target <= 0) {
-            return 0;
-        }
-
-        // Check if the result is already memoized
-        std::string key = std::to_string(d) + "_" + std::to_string(target);
-        if (memo.find(key) != memo.end()) {
-            return memo[key];
-        }
-
-        int ways = 0;
-        for (int i = 1; i <= f; ++i) {
-            ways = (ways + helper(d - 1, f, target - i, memo)) % MOD;
-        }
-
-        // Memoize the result
-        memo[key] = ways;
-        return ways;
+        
+        return dp[target];
     }
 };
