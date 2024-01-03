3d0dfe3dad55823246fef501293a905a1eb9a5b3
Li-Wei Yang <68542467+liver121888@users.noreply.github.com>
2024-01-03 12:40:54 +0800

    Time: 237 ms (5.06%), Space: 29.1 MB (34.88%) - LeetHub

---
+++ 2125-number-of-laser-beams-in-a-bank/2125-number-of-laser-beams-in-a-bank.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int numberOfBeams(vector<string>& bank) {
+        int lastRowCnt = -1;
+        int curRowCnt{};
+        int ans = 0;
+        for (auto row : bank) {
+            curRowCnt = 0;
+            for (auto c : row)
+                if (c == '1')
+                    curRowCnt++;
+       
+            if (lastRowCnt == -1) {
+                lastRowCnt = curRowCnt;
+                continue;
+            }
+
+            if(curRowCnt == 0)
+                continue;
+
+            ans += (lastRowCnt * curRowCnt);
+            lastRowCnt = curRowCnt;
+            
+            
+        }
+        return ans;
+    }
+};
