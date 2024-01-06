db6f85415ab756ae2c5eb6be0fdfe3a9a7fcca11
Li-Wei Yang <68542467+liver121888@users.noreply.github.com>
2024-01-07 00:38:08 +0800

    Time: 33 ms (51.53%), Space: 21.4 MB (29.46%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,10 +1,6 @@
 class Solution {
 public:
-    
-    bool myCmp (string i,string j) { 
-        return (i.length()<j.length()); 
-    }
-    
+
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         vector<vector<string>> ans; 
         unordered_map<string,vector<string>> umap; 
