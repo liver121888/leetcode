79bb7ba94ff28e58a966e1291a7f4c69fb513790
Li-Wei Yang <68542467+liver121888@users.noreply.github.com>
2024-01-09 07:54:55 +0800

    Time: 5 ms (40.78%), Space: 8.3 MB (24.25%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -5,21 +5,33 @@ public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         m = obstacleGrid.size();
         n = obstacleGrid[0].size();
-        vector<vector<int>> dp(m + 1, vector<int>(n + 1));
+        // forward dp
+        vector<vector<int>> dp(m, vector<int>(n));
         
         //base case
-        dp[1][1] = 1;
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (obstacleGrid[i - 1][j - 1] == 1) {
+        dp[0][0] = 1;
+        int top{}; int left{};
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (obstacleGrid[i][j] == 1) {
                     dp[i][j] = 0;
                 } else {
-                    dp[i][j] += dp[i - 1][j] + dp[i][j - 1];  
+                    if (i - 1 < 0)
+                        top = 0;
+                    else
+                        top = dp[i - 1][j];
+                    if (j - 1 < 0)
+                        left = 0;
+                    else
+                        left = dp[i][j - 1];
+                    
+                    dp[i][j] += top + left;  
                 }                
             }
         }
-        return dp[m][n];
+        return dp[m - 1][n - 1];
         
+        // with bottom up dfs + memo (I call this backward dp), TLE
         // vector<vector<int>> dp(m, vector<int>(n, -1));
         // return helper(obstacleGrid, dp, m - 1, n - 1);   
     }
