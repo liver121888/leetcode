7ae9549b26d6340b27a84ab58a76145ab08f9470
Li-Wei Yang <68542467+liver121888@users.noreply.github.com>
2024-01-11 15:06:04 +0800

    Time: 19 ms (57.20%), Space: 23.6 MB (63.41%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -11,6 +11,11 @@ public:
         //     right--;
         // }
         
-        reverse(s.begin(), s.end());
+        // reverse(s.begin(), s.end());
+        
+        for (int i = 0; i < s.size()/2; i++) {
+            swap(s[i], s[s.size() - 1 - i]);
+        }
+        
     }
 };
