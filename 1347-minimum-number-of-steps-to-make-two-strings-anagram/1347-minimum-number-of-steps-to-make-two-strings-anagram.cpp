6b5871c9653610dd47a2080893cc9f422883692e
Li-Wei Yang <68542467+liver121888@users.noreply.github.com>
2024-01-13 14:27:04 +0800

    Time: 49 ms (75.61%), Space: 16.9 MB (62.45%) - LeetHub

--- 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.cpp
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.cpp
@@ -2,20 +2,17 @@ class Solution {
 public:
     int minSteps(string s, string t) {
         vector<int> sCnts(26, 0);
-        for (auto c : s) {
-            sCnts[c - 'a']++;
-        }
         
-        for (auto c : t) {
-            sCnts[c - 'a']--;
+        for (int i = 0; i < s.size(); i++) {
+            sCnts[t[i] - 'a']++;
+            sCnts[s[i] - 'a']--;
         }
-        
+
         int res = 0;
         for (auto cnt : sCnts) {
             if (cnt > 0)
                 res += cnt;
         }
         return res;
-        
     }
 };
