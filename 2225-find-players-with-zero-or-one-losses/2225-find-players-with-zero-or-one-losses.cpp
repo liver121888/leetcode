394d2fd326a0ef48bc06b1990f0bd9aef8d7dcd7
Li-Wei Yang <68542467+liver121888@users.noreply.github.com>
2024-01-15 13:43:43 +0800

    Time: 481 ms (72.49%), Space: 181.1 MB (37.13%) - LeetHub

--- 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.cpp
@@ -2,22 +2,34 @@ class Solution {
 public:
     vector<vector<int>> findWinners(vector<vector<int>>& matches) {
         unordered_map<int, int> loseCnts;
-        sort(matches.begin(), matches.end());
+        // sort(matches.begin(), matches.end());
         for (auto match : matches) {
-            if (loseCnts.find(match[0]) == loseCnts.end())
-                loseCnts[match[0]] = 0;
             loseCnts[match[1]]++;
         }
         
-        vector<vector<int>> ans(2);
-        for (auto cnt : loseCnts) {
-            if (cnt.second ==0)
-                ans[0].push_back(cnt.first);
-            else if (cnt.second == 1)
-                ans[1].push_back(cnt.first);
+        vector<int> noLose;
+        vector<int> oneLose;
+        
+        for(int i = 0; i < matches.size(); i++)
+        {
+           int winner = matches[i][0];
+           int looser = matches[i][1];
+           if(loseCnts.find(winner) == loseCnts.end()) {
+               noLose.push_back(winner);
+               loseCnts[winner] = 2;
+           }
+           if(loseCnts[looser] == 1)
+               oneLose.push_back(looser);
         }
-        sort(ans[0].begin(), ans[0].end());
-        sort(ans[1].begin(), ans[1].end());
-        return ans;
+        
+        // for (auto cnt : loseCnts) {
+        //     if (cnt.second == 0)
+        //         noLose.push_back(cnt.first);
+        //     else if (cnt.second == 1)
+        //         oneLose.push_back(cnt.first);
+        // }
+        sort(noLose.begin(), noLose.end());
+        sort(oneLose.begin(), oneLose.end());
+        return {noLose, oneLose};
     }
 };
