adb8b12a246c6659d686f5d46ac106cebc56ffed
Li-Wei Yang <68542467+liver121888@users.noreply.github.com>
2024-02-05 16:00:22 +0800

    Time: 19 ms (90.75%), Space: 11.8 MB (27.34%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.cpp
@@ -1,20 +1,15 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
-        vector<int> memo(26, s.length() + 1);
-        
-        for (int i = 0; i < s.length(); i++) {
-            if (memo[s[i] - 'a'] == s.length() + 1)
-                memo[s[i] - 'a'] = i;
-            else
-                memo[s[i] - 'a'] = s.length();
-        }
-        
-        int curFirst = s.length();
-        for (auto cnt : memo) {
-            curFirst = min(cnt, curFirst);
-        }
+        vector<int> oc(26);
 
-        return curFirst == s.length() ? -1 : curFirst;
+        for (auto i : s) 
+            oc[i - 'a']++;
+
+        for (int i = 0; i < s.size(); i++) {
+            if (oc[s[i] - 'a'] == 1) 
+                return i;
+        }
+        return -1;
     }
 };
