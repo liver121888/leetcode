bfb92ec9ac2dafb918fe13999db66fe72295cf05
Li-Wei Yang <68542467+liver121888@users.noreply.github.com>
2024-12-01 00:14:47 -0500

    Time: 0 ms (100.00%), Space: 13.8 MB (16.30%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
@@ -5,7 +5,7 @@ public:
         unordered_map<int, int> aMap;
         for (int i = 0; i < arr.size(); i++) {
 
-            if (arr[i] % 2 == 0 && aMap.find(arr[i]/2) != aMap.end() && i != aMap[arr[i]])
+            if (arr[i] % 2 == 0 && aMap.find(arr[i]/2) != aMap.end())
                 return true;
             
             if (aMap.find(2 * arr[i]) != aMap.end())
