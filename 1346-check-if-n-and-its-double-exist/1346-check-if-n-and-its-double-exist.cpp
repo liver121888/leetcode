d4b360c8678ff279c0a1175ff185be223aa5d2f0
Li-Wei Yang <68542467+liver121888@users.noreply.github.com>
2024-12-01 00:14:39 -0500

    Time: 0 ms (100.00%), Space: 13.8 MB (15.78%) - LeetHub

---
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) {
+        
+        unordered_map<int, int> aMap;
+        for (int i = 0; i < arr.size(); i++) {
+
+            if (arr[i] % 2 == 0 && aMap.find(arr[i]/2) != aMap.end() && i != aMap[arr[i]])
+                return true;
+            
+            if (aMap.find(2 * arr[i]) != aMap.end())
+                return true;
+            
+            aMap[arr[i]] = i;
+        }
+        return false;
+        
+    }
+};
