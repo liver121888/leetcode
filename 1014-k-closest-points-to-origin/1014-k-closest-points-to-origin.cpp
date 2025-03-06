88ea99f489d8bd6f811a61e9e764a9c23178e848
Li-Wei Yang <68542467+liver121888@users.noreply.github.com>
2025-03-06 13:30:33 -0500

    Time: 37 ms (69.93%), Space: 66.8 MB (82.18%) - LeetHub

--- 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.cpp
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.cpp
@@ -6,19 +6,31 @@ public:
         // total n
 
         // dist, idx
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int,int>>> pq;
+        // priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int,int>>> pq;
+        priority_queue<pair<int, int>> maxPQ;
 
         for (int i = 0; i < points.size(); i++) {
             int dist = pow(points[i][0], 2) + pow(points[i][1], 2);
-            pq.push({dist, i});
+            // pq.push({dist, i});
+            if (maxPQ.size() < k) {
+                maxPQ.push({dist, i});
+            } else if (dist < maxPQ.top().first) {
+                maxPQ.pop();
+                maxPQ.push({dist, i});
+            }
         }
 
         vector<vector<int>> result;
-        while (k > 0) {
-            int idx = pq.top().second;
-            pq.pop();
-            result.push_back(points[idx]);
-            k--;
+        // while (k > 0) {
+        //     int idx = pq.top().second;
+        //     pq.pop();
+        //     result.push_back(points[idx]);
+        //     k--;
+        // }
+        while (!maxPQ.empty()) {
+            int entryIndex = maxPQ.top().second;
+            maxPQ.pop();
+            result.push_back(points[entryIndex]);
         }
         return result;
     }
