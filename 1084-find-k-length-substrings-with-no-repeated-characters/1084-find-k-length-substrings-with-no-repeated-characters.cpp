52bde5f2ce1683562d2f03f46ea88f91e73a491e
Li-Wei Yang <68542467+liver121888@users.noreply.github.com>
2025-03-11 00:51:12 -0400

    Time: 123 ms (6.49%), Space: 8.9 MB (69.05%) - LeetHub

---
+++ 1084-find-k-length-substrings-with-no-repeated-characters/1084-find-k-length-substrings-with-no-repeated-characters.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+
+    bool checkValid(const vector<int>& freq) {
+        for (auto f : freq) {
+            if (f > 1)
+                return false;
+        }
+        return true;
+    }
+
+    int numKLenSubstrNoRepeats(string s, int k) {
+
+        // of len k -> sliding window
+        // freq
+
+        // deal with edge case 
+        // if (k > s.length())
+        //     return 0;
+
+        auto freq = vector<int>(26);
+        int l = 0; int result = 0;
+        for (int r = 0; r < s.length(); r++) {
+
+            freq[s[r] - 'a']++;
+
+            // retract
+            if (r - l + 1 > k) {
+                freq[s[l] - 'a']--;
+                l++;
+            }
+
+            if (checkValid(freq) && r - l + 1 == k) {
+                result++;
+                cout << "l " << l << " r " << r << " result " << result << endl;
+            }
+
+            // expand
+        }
+        return result;
+    }
+};
