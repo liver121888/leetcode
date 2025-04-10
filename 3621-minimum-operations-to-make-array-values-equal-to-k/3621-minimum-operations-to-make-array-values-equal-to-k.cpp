cb74b725160e11a10b7e34f7d8dc2a2d8296d293
Li-Wei Yang <68542467+liver121888@users.noreply.github.com>
2025-04-10 13:07:47 -0400

    Time: 12 ms (54.72%), Space: 31.7 MB (73.9%) - LeetHub

---
+++ 3621-minimum-operations-to-make-array-values-equal-to-k/3621-minimum-operations-to-make-array-values-equal-to-k.cpp
@@ -0,0 +1,67 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) {
+
+        // sequence is not important
+        // each operation: select h that is valid, set all number to h
+
+        // [2 4 5 5 5], k = 2
+        // need minOp
+        // if k is possible, select k directly
+        // select 4 -> [2 4 4 4 4]
+        // select 2 -> [2 2 2 2 2]
+
+        // [2 4 5 5 6]
+        // we cannot select 4 because there is one more
+
+        // [2 1 2], k = 2
+        // -1 because impossible
+
+        // sort(nums.begin(), nums.end());
+
+        // cnts -> freq of all the elements
+
+        // set all the largest number to second largest number
+        // repeat until we set all numbers to k
+
+        // 1 <= nums[i] <= 100
+        vector<int> cnts(100 + 1);
+
+        // calc the freq
+        for (auto num : nums) {
+            cnts[num] += 1;
+        }
+        
+        int ans = 0;
+        for (int i = 1; i < cnts.size(); i++) {
+
+            if (cnts[i] != 0) {
+                if (i < k) {
+                    return -1;
+                } else if (i > k) {
+                    ans++;
+                } 
+                // do nothing if i == k
+            }
+
+        }
+
+        // [2 4 5 5 5], k = 2
+        // 1 1 3
+        // i == 4
+        // ans = 1
+        // i == 5
+        // ans = 2
+
+        // [2 1 2], k = 2
+        // i == 1, return -1
+
+        // [9 7 5 3]
+        // 1 1 1 1 k = 1
+        // 3 -> ans++;
+        // 5 -> ans++;
+        // return 4
+
+        return ans;
+    }
+};
