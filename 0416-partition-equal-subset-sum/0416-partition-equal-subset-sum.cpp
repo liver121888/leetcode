244a505d41aee7a6dbb1eabad5aa86a4e0cf6c7e
Li-Wei Yang <68542467+liver121888@users.noreply.github.com>
2026-01-04 12:56:52 -0800

    Time: 39 ms (94.2%), Space: 111.2 MB (18.39%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -22,7 +22,9 @@
 class Solution {
 public:
 
-    // TLE
+    // TLE, this solution does no capture the top-down dp idea
+    // it just backtrack to the end of the array
+    // 絕對算是 Backtracking（回溯法）的一種形式，更精確地說，它是暴力遞迴（Brute Force Recursion）。
 
     // int totalSum = 0;
     // int n = 0;
@@ -59,7 +61,9 @@ public:
     // }
 
 
-
+    // Top-down DP
+    // Time: O(mn)
+    // Space: O(mn)
     bool canPartition(vector<int> &nums) {
         int totalSum = 0;
         for (int num : nums) {
@@ -74,6 +78,7 @@ public:
         // 修改點 1: 使用 int 矩陣，並初始化為 -1
         vector<vector<int>> memo(n + 1, vector<int>(subSetSum + 1, -1));
         
+        // Top-down dfs with memo
         return dfs(nums, n - 1, subSetSum, memo);
     }
 
@@ -93,5 +98,34 @@ public:
         // 修改點 4: 儲存時，bool 會自動轉為 1 或 0
         return memo[n][subSetSum] = result;
     }
+
+
+    // bottom-up
+    // bool canPartition(vector<int> &nums) {
+    //     int totalSum = 0;
+    //     // find sum of all array elements
+    //     for (int num : nums) {
+    //         totalSum += num;
+    //     }
+    //     // if totalSum is odd, it cannot be partitioned into equal sum subset
+    //     if (totalSum % 2 != 0) return false;
+    //     int subSetSum = totalSum / 2;
+    //     int n = nums.size();
+    //     bool dp[n + 1][subSetSum + 1];
+    //     memset(dp, 0, (n + 1) * (subSetSum + 1) * sizeof(bool));
+    //     dp[0][0] = true;
+    //     for (int i = 1; i <= n; i++) {
+    //         int curr = nums[i - 1];
+    //         for (int j = 0; j <= subSetSum; j++) {
+    //             if (j < curr)
+    //                 dp[i][j] = dp[i - 1][j];
+    //             else
+    //                 dp[i][j] = dp[i - 1][j] || (dp[i - 1][j - curr]);
+    //         }
+    //     }
+    //     return dp[n][subSetSum];
+    // }
+
+
 
 };
