4068550158d3e2d9fa29c2a4c845199a16a9f3b2
Li-Wei Yang <68542467+liver121888@users.noreply.github.com>
2026-01-05 13:24:33 -0800

    Time: 0 ms (100%), Space: 31.3 MB (44.31%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int l = 0;
-        int r = nums.size() - 1;
-        int mid{};
+        // [-1,0,3,5,9,12]
+        int l = 0, r = nums.size()-1;
         while (l <= r) {
-            mid = (l + r) / 2;
-            if (nums[mid] > target)
-                r = mid - 1;
-            else if (nums[mid] < target)
-                l = mid + 1;
-            else
+            int mid = l + (r - l)/2;
+            int v = nums[mid];
+            if (v == target) {
                 return mid;
+            } else if (v < target) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
         }
         return -1;
+
     }
 };
