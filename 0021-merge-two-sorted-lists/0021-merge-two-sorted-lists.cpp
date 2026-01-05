eabd93151eefe5a6950c07bb5b96a4f070c911f3
Li-Wei Yang <68542467+liver121888@users.noreply.github.com>
2026-01-05 15:05:58 -0800

    Time: 0 ms (100%), Space: 19.5 MB (27.8%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -10,27 +10,93 @@
  */
 class Solution {
 public:
+
+    // ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+
+    //     ListNode* start1 = list1;
+    //     ListNode* start2 = list2;
+
+    //     if (!start1) return start2;
+    //     if (!start2) return start1;
+
+    //     ListNode* head;
+    //     ListNode* first_head;
+
+    //     if (start1->val < start2->val) {
+    //         head = start1;
+    //         first_head = start1;
+    //         start1 = start1->next;
+    //     } else {
+    //         head = start2;
+    //         first_head = start2;
+    //         start2 = start2->next;
+    //     }
+
+    //     while (start1 && start2) {
+    //         if (start1->val < start2->val) {
+    //             first_head->next = start1;
+    //             start1 = start1->next;
+    //         } else {
+    //             first_head->next = start2;
+    //             start2 = start2->next;
+    //         }
+    //         first_head = first_head->next;
+    //     }
+
+    //     if (start1) {
+    //         first_head->next = start1;
+    //     } else {
+    //         first_head->next = start2;
+    //     }
+
+    //     return head;
+    // }
+
+    // both list are already sorted
+    // we can always compare the current node and link the smaller one
+    // after the operations we might left with a part, append that part
+    // time complexity: O(n)
+    // space complexity: O(1)
+    // some special case: empty list
+
+    // I will do my solution here
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode* head = new ListNode();
-        ListNode* current = head;
         
-        while (list1 != 0 && list2 != 0) {
-            if (list1->val > list2->val) {
-                current->next = list2;
-                current = current->next;
-                list2 = list2->next;                
+        
+        // list1 = [1,2,4], list2 = [1,3,4]
+        
+        ListNode* dummy = new ListNode();
+        ListNode* head = dummy;
+        while (list1 && list2) {
+            if (list1->val < list2->val) {
+                // connect main list to list1
+                dummy->next = list1;
+                // advance list1 ptr
+                list1 = list1->next;
+                // disconnect main list from list1
+                dummy->next->next = nullptr;
             } else {
-                current->next = list1;
-                current = current->next;
-                list1 = list1->next;                
-            }            
+                // connect main list to list2
+                dummy->next = list2;
+                // advance list2 ptr
+                list2 = list2->next;
+                // disconnect main list from list2
+                dummy->next->next = nullptr;
+            }
+            
+            // progress the main list
+            dummy = dummy->next;
+        } 
+        
+        if (list1) {
+            dummy->next = list1;            
         }
         
-        if (list1 != 0)
-            current->next = list1;
-        else if (list2 != 0)
-            current->next = list2;
-            
+        if (list2) {
+            dummy->next = list2;            
+        }
+
         return head->next;
+        
     }
 };
