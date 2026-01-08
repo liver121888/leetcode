89a4d5a20fe0c3ad96efd5f7b64069ace145be06
Li-Wei Yang <68542467+liver121888@users.noreply.github.com>
2026-01-08 10:40:48 -0800

    Time: 0 ms (100%), Space: 23.9 MB (54.11%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,17 +1,47 @@
+// we have to do it in-place
+// we can solve using two pointers
+// [0,1,0,3,12]
+// l r two pointers
+// if nums[l] is 0
+// we need to use r to find non zero and swap
+// if nums[l] is not 0
+// we move the r and l pointer tgt
+
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int left = 0;
-        int right = 0;
-        while (right < nums.size()) {
-            if (nums[right] != 0) {
-                swap(nums[left], nums[right]);
-                left++;      
-                right++;
+
+        int l = 0, r = 0;
+        while (r < nums.size()) {
+            if (nums[l] != 0) {
+                l++;
+                r++;
+            } else {
+                while (r < nums.size() && nums[r] == 0) {
+                    r++;
+                }
+                if (r >= nums.size())
+                    break;
+                // swap
+                nums[l] = nums[r];
+                nums[r] = 0;
+                l++;
+                r++;
             }
-            else
-                right++;            
         }
-        return;
+
+        // [0,1,0,3,12], n = 5
+        // l = 0, r = 1
+        // [1, 0], l = 1 r = 2
+        // [1, 0, 0, 3, 12]
+        // [1, 3, 0, 0, 12]
+        // [1, 3, 12, 0, 0]
+
+
+        // [1, 3, 5, 6, 7]
+
+
+
+        
     }
 };
