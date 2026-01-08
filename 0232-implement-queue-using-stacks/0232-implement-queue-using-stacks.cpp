47ecdf4917b6ffdedc3e22473b81c65d15a400df
Li-Wei Yang <68542467+liver121888@users.noreply.github.com>
2026-01-07 20:54:32 -0800

    Time: 0 ms (100%), Space: 9.6 MB (95.5%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.cpp
@@ -1,41 +1,74 @@
+// I can use two stack to simulate a queue
+// when push, push to first stack
+// [1], [2]
+// pop should be 1
+
+
+
 class MyQueue {
 public:
+
     stack<int> st1;
     stack<int> st2;
-    int front;
-    MyQueue() {
 
+    MyQueue() {
+        
     }
     
     void push(int x) {
-        if (st1.empty())
-            front = x;
+
         st1.push(x);
+        
     }
     
     int pop() {
+
+        while (!st1.empty()) {
+            int data = st1.top();
+            st1.pop();
+            st2.push(data);
+        }
         
-        if (st2.empty()) {
-            while(!st1.empty()) {
-                st2.push(st1.top());
-                st1.pop();
-            }   
+        int result = st2.top();
+        st2.pop();
+
+        while (!st2.empty()) {
+            int data = st2.top();
+            st2.pop();
+            st1.push(data);
         }
 
-        int temp = st2.top();
-        st2.pop();
-        return temp;
+        return result;
+        
     }
     
     int peek() {
-        if (!st2.empty()) {
-            return st2.top();
+
+        while (!st1.empty()) {
+            int data = st1.top();
+            st1.pop();
+            st2.push(data);
         }
-        return front;
+        
+        int result = st2.top();
+
+        while (!st2.empty()) {
+            int data = st2.top();
+            st2.pop();
+            st1.push(data);
+        }
+
+        return result;
+
+        
     }
     
     bool empty() {
-        return st1.empty() && st2.empty();
+
+        if (!st1.empty())
+            return false;
+        return true;
+        
     }
 };
 
