4b81878ac425dc4551430a3122650b7782b448ef
Li-Wei Yang <68542467+liver121888@users.noreply.github.com>
2026-01-12 15:10:00 -0800

    Time: 0 ms (100%), Space: 13.9 MB (8.54%) - LeetHub

--- 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.cpp
+++ 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.cpp
@@ -1,26 +1,33 @@
+// we need to visit the points by order
+// we can move diagonally or horizontally or vertically
+// we should move diagonally until we have to move horizontal or vertical
+// we don't need to simulate, there is a defined solution for this question
+// [[1,1],[3,4],[-1,0]]
+
+// abs(3 - 1) = 2, abs(4 - 1) = 3
+// we take the max = 3
+// abs(-1 - 3) = 4, 0 - 4 = 4
+// we take the max = 4
+
+// 3 + 4 = 7
+// [[3,2],[-2,2]]
+// 5
+
+// what are some edge cases?
+// if n == 1
+// ans = 0
+
 class Solution {
 public:
     int minTimeToVisitAllPoints(vector<vector<int>>& points) {
-        int ans = 0;
         int n = points.size();
-        int diffx = 0;
-        int diffy = 0;
+        if (n == 1)
+            return 0;
+        
+        int ans = 0;
         for (int i = 1; i < n; i++) {
-            diffx = points[i][0] - points[i - 1][0];
-            diffy = points[i][1] - points[i - 1][1];
-            while ((abs(diffx) + abs(diffy)) > 0) {
-                if (abs(diffx) > 0 && abs(diffy) > 0) {
-                    // take diagonal
-                    diffx += signbit(diffx) ? 1 : -1;                    
-                    diffy += signbit(diffy) ? 1 : -1;
-                } else {
-                    if (abs(diffx) == 0)
-                        diffy += signbit(diffy) ? 1 : -1;  
-                    else
-                        diffx += signbit(diffx) ? 1 : -1;  
-                }                
-                ans += 1;
-            }
+            ans += max(abs(points[i][0] - points[i-1][0]), 
+                abs(points[i][1] - points[i-1][1]));
         }
         return ans;
     }
