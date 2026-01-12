7cb3e4c822c28fda9bfa0d60986b17da07da3164
Li-Wei Yang <68542467+liver121888@users.noreply.github.com>
2026-01-12 15:10:46 -0800

    Time: 0 ms (100%), Space: 13.9 MB (26.71%) - LeetHub

--- 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.cpp
+++ 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.cpp
@@ -19,16 +19,31 @@
 
 class Solution {
 public:
+
+    // official solution
     int minTimeToVisitAllPoints(vector<vector<int>>& points) {
-        int n = points.size();
-        if (n == 1)
-            return 0;
-        
         int ans = 0;
-        for (int i = 1; i < n; i++) {
-            ans += max(abs(points[i][0] - points[i-1][0]), 
-                abs(points[i][1] - points[i-1][1]));
+        for (int i = 0; i < points.size() - 1; i++) {
+            int currX = points[i][0];
+            int currY = points[i][1];
+            int targetX = points[i + 1][0];
+            int targetY = points[i + 1][1];
+            ans += max(abs(targetX - currX), abs(targetY - currY));
         }
+        
         return ans;
     }
+
+    // int minTimeToVisitAllPoints(vector<vector<int>>& points) {
+    //     int n = points.size();
+    //     if (n == 1)
+    //         return 0;
+        
+    //     int ans = 0;
+    //     for (int i = 1; i < n; i++) {
+    //         ans += max(abs(points[i][0] - points[i-1][0]), 
+    //             abs(points[i][1] - points[i-1][1]));
+    //     }
+    //     return ans;
+    // }
 };
