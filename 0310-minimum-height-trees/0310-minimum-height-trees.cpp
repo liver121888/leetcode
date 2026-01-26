49f69bceb9e865f8efd1f4f627026e87f34dafb6
Li-Wei Yang <68542467+liver121888@users.noreply.github.com>
2026-01-25 23:24:48 -0800

    Time: 51 ms (43.95%), Space: 58 MB (53.95%) - LeetHub

--- 0310-minimum-height-trees/0310-minimum-height-trees.cpp
+++ 0310-minimum-height-trees/0310-minimum-height-trees.cpp
@@ -1,31 +1,12 @@
-// build adj list
-// choose different node as root and use bfs to calculate the depth
-// O(n * n)
-
-// how can we make a tree minimum height
-// select the most out degree one and make it either 
-// left right root?
-// how can we prove this is right?
-
-// so in degree and out degree is same here
-// we should take the most edged node
-// try as root, and try as child
-
-// we can prove that a most edged node cannot be on the second layer?
-
 class Solution {
 public:
     vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
+        if (n == 1) return {0}; // Edge case: single node
 
-        if (n < 2) {
-            vector<int> centroids;
-            for (int i = 0; i < n; i++)
-                centroids.push_back(i);
-            return centroids;
-        }
-
-        vector<vector<int>> adjLists(n, vector<int>());
+        vector<vector<int>> adjLists(n);
         vector<int> degrees(n, 0);
+
+        // Build Graph and calculate initial degrees
         for (auto& edge : edges) {
             adjLists[edge[0]].push_back(edge[1]);
             adjLists[edge[1]].push_back(edge[0]);
@@ -33,36 +14,31 @@ public:
             degrees[edge[1]]++;
         }
 
-
         // Initialize the first layer of leaves
         vector<int> leaves;
-        for (int i = 0; i < n; i++)
-            if (degrees[i] == 1)
+        for (int i = 0; i < n; i++) {
+            if (degrees[i] == 1) {
                 leaves.push_back(i);
+            }
+        }
 
-
+        // Trim leaves until we reach the centroid(s)
         int remainingNodes = n;
-        vector<bool> visited(n);
         while (remainingNodes > 2) {
             remainingNodes -= leaves.size();
-
             vector<int> newLeaves;
 
-            for (auto& leaf : leaves) {
-                visited[leaf] = true;
-
-                for (auto& neighbor : adjLists[leaf]) {
-
-                    if (!visited[neighbor]) {
-                        degrees[neighbor]--;
-                        // if we have a new leaf
-                        if (degrees[neighbor] == 1)
-                            newLeaves.push_back(neighbor);
-
+            for (int leaf : leaves) {
+                for (int neighbor : adjLists[leaf]) {
+                    // We simply decrement the neighbor's degree. 
+                    // We don't need 'visited' because removed nodes 
+                    // won't ever satisfy the (degree == 1) condition again.
+                    degrees[neighbor]--;
+                    
+                    if (degrees[neighbor] == 1) {
+                        newLeaves.push_back(neighbor);
                     }
-
                 }
-
             }
             leaves = newLeaves;
         }
