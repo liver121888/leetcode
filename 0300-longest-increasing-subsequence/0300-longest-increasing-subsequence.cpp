1b51c6bbbcb5e9c28e24271f6dca3743918262bd
Li-Wei Yang <68542467+liver121888@users.noreply.github.com>
2026-01-28 11:41:59 -0800

    Time: 103 ms (42.66%), Space: 14.4 MB (62.84%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,16 +1,36 @@
 class Solution {
 public:
-    int lengthOfLIS(vector<int>& nums) {
-        vector<int> dp(nums.size(), 1);
+    int dfs(const vector<int>& nums, vector<int>& memo, int i) {
+        // Base case: 雖然你寫了 if(i==0) return 1
+        // 但其實下面的 loop 跑不進去時，預設回傳 1 也是對的 (自己就是長度1)
+        
+        if (memo[i] != 0) return memo[i];
 
-        for (int i = nums.size() - 1; i >= 0 ; i--) {
-            for (int j = i + 1; j < nums.size(); j++) {
-                if (nums[i] < nums[j])
-                    dp[i] = max(dp[i], 1 + dp[j]);
+        int maxLen = 1; // 至少包含自己
+        
+        // 往前找所有可能接在後面的元素
+        for (int j = 0; j < i; j++) {
+            if (nums[i] > nums[j]) {
+                maxLen = max(maxLen, dfs(nums, memo, j) + 1);
             }
-            
         }
-                    
-        return *max_element(dp.begin(), dp.end());
+
+        memo[i] = maxLen;
+        return maxLen;
+    }
+
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 0) return 0;
+        
+        vector<int> memo(n, 0);
+        
+        // 修正點：LIS 可能結束在任何位置，所以要遍歷所有位置
+        int globalMax = 1;
+        for (int i = 0; i < n; i++) {
+            globalMax = max(globalMax, dfs(nums, memo, i));
+        }
+        
+        return globalMax;
     }
 };
