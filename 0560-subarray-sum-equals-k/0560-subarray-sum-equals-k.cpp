a1a9c6cf2eaa40cef2d83bbbac4d4ab208617467
Li-Wei Yang <68542467+liver121888@users.noreply.github.com>
2026-01-29 12:09:58 -0800

    Time: 1970 ms (18.49%), Space: 37.3 MB (78.33%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,23 +1,69 @@
+// this is a subarray problem
+// we can solve using two pointers
+// we can calculate prefix sum
+// [1, 1, 1]
+// [1, 2, 3]
+// so we can come up with each subarray sum
+// [1,2,3], k = 3
+// [1, 3, 6]
+// we traverse throught once, to see if they equal to k
+// then 
+
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        unordered_map<int, int> aMap;
-        aMap[0] = 1;
-        int ans = 0;
-        int curSum = 0;
-        int target = 0;
+
+        vector<int> prefixSum;
+        prefixSum.push_back(0);
+        int currSum = 0;
         for (auto num : nums) {
-            curSum += num;
-            target = curSum - k;
-            if (aMap.find(target) != aMap.end())
-                ans += aMap[target];
-            aMap[curSum]++;
+            currSum += num;
+            prefixSum.push_back(currSum);
         }
-        
-        // for (auto& [k, v] : aMap) {
-        //     cout << k << ' ' << v << endl;
+        int count = 0;
+        for (int start = 0; start < nums.size(); start++) {
+            for (int end = start + 1; end <= nums.size(); end++) {
+                if (prefixSum[end] - prefixSum[start] == k)
+                    count++;
+            }
+        }
+
+        return count;
+
+        // time: O(n^2);
+        // space: O(n);
+
+
+
+        // -1000 <= nums[i] <= 1000
+        // then we cannot use two pointers cuz shrink not necessnary decrease
+
+        // int l = 0, curr = 0, cnt = 0;
+        // for (int r = 0; r < nums.size(); r++) {
+        //     curr += nums[r];
+        //     while (curr > k && l <= r) {
+        //         // shrink
+        //         curr -= nums[l];
+        //         l++;
+        //     }
+
+        //     if (curr == k)
+        //         cnt += 1;
+
+        //     // expand
         // }
-        
-        return ans;
+        // return cnt;
+
+        // [1, 1, 1], k = 2
+        // l = 0, curr = 0, cnt = 0, r = 0;
+        // curr = 1
+        // l = 0, curr = 1, cnt = 0, r = 1;
+        // curr = 2, cnt = 1
+        // l = 0, curr = 1, cnt = 0, r = 2;
+        // curr = 2, cnt = 1
+        // cnt = 2, return 2
+        // nums = [1,2,3], k = 3
+        // nums = [100], k = 0
+
     }
 };
