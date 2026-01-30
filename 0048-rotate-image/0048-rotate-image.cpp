f3940b6bafa3c8591617530ffe2087ed14cd5fa0
Li-Wei Yang <68542467+liver121888@users.noreply.github.com>
2026-01-29 16:37:49 -0800

    Time: 0 ms (100%), Space: 10.1 MB (69.15%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,57 +1,44 @@
+// matrix is always square
+// we take the index i, j
+// let new matrix be mat_n, old matrix is m
+// m[0][0] -> mat_n[0][n-1]
+// m[1][2] -> mat_n[2][2]
+// m[i][j] -> mat_n[n-j][]
+// we can view it as 5, 11, 16, 15 as a group
+// we move it 
+
+// n-1 = 3
+// [0][0] <- [3][0] <- [3][3] <- [0][3]
+// [0][1] <- [2][0] <- [2][3] <- [1][3]
+// [0][2] <- [1][0] <- [1][3] <- [2][3]
+// [0][3] <- [0][0] <- [0][3] <- [3][3]
+
+// [1][1] <- [2][1] <- [2][2] <- [1][2]
+
+
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-
-        // curStep 0 -> n-2
-        // cur layer
-
-        // 0 0 -> 0 n-1
-        // 0 n-1 -> n-1 n-1
-        // n-1 n-1 -> n-1 0
-        // n-1 0 -> 0 0
-
-        // 0 1 -> 1 n-1
-        // 1 n-1 -> n-1 1
-        // n-1 1 -> 1 0
-        // 1 0 -> 0 1
-
-        // 1 1
-
         int n = matrix.size();
-
-        for (int i = 0; i < (n + 1)/2; i++) {
-            for ( int j = 0; j < n/2; j++) {
-                int temp = matrix[n - 1 - j][i];
-                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - j - 1];
-                matrix[n - 1 - i][n - j - 1] = matrix[j][n - 1 - i];
-                matrix[j][n - 1 - i] = matrix[i][j];
-                matrix[i][j] = temp;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i; j < n - 1 - i; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[n-1-j][i];
+                matrix[n-1-j][i] = matrix[n-1-i][n-1-j];
+                matrix[n-1-i][n-1-j] = matrix[j][n-1-i];
+                matrix[j][n-1-i] = tmp;
             }
         }
-
-        // curLayer = 0, curStep = 0, n = 3
-        // n/2 = 1, n-1 = 2
-        // z = 1
-        // (2, 0) = 7
-        // (2, 2) = 9
-        // (0, 2) = 3
-        // 7 2 1
-        // 4 5 6
-        // 9 8 3
-
-        // curLayer = 0, curStep = 1, n = 3
-        // n/2 = 1, n-1 = 2
-        // z = 2
-        // (2, 1) = 7
-        // (2, 2) = 9
-        // (0, 2) = 3
-        // 7 2 1
-        // 4 5 6
-        // 9 8 3
-
-
-
-
-
     }
+
+    // i = 0, j = 0
+    // mat[0][0], mat[2][0], mat[2][2], mat[0][2]
+    // i = 1 j = 0
+
+    // [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
+    // i = 0, j = 0
+    // mat[0]0], mat[3][0], mat[3][3], mat[0][3]
+    // i = 1 j = 0
+
+
 };
