1e28a442a5a0e9f3392e31f36d260ec6a7c086cc
Li-Wei Yang <68542467+liver121888@users.noreply.github.com>
2026-02-04 10:27:52 -0800

    Time: 0 ms (100%), Space: 12.1 MB (61.87%) - LeetHub

---
+++ 0419-battleships-in-a-board/0419-battleships-in-a-board.cpp
@@ -0,0 +1,48 @@
+// we can use bfs to find the connected component
+// this will take O(n^2) time and O(1) space if we 
+// marked visited in place
+
+// we want a one-pass solution and only O(1) memory but 
+// cannot modify the board
+// one of the property we can use is that the ship 
+// is either horizontal or vertical
+// we can try first traverse the board, if we find an X, 
+// check 4 dirs
+// this is still using bfs, I think we can do is group the 
+// X by row and col
+
+
+class Solution {
+public:
+    int countBattleships(vector<vector<char>>& board) {
+        int n = board.size();
+        int m = board[0].size();
+        int ans = 0;
+        for (int r = 0; r < n; r++) {
+            for (int c = 0; c < m; c++) {
+                if (board[r][c] == 'X') {
+                    // if (r == 0)
+                    //     ans++;
+                    // else if (c == 0)
+                    //     ans++;
+                    // else if (board[r][c-1] == '.' && board[r-1][c] == '.')
+                    //     ans++;
+
+                    bool hasUp = (r > 0 && board[r-1][c] == 'X');
+                    bool hasLeft = (c > 0 && board[r][c-1] == 'X');
+                    if (!hasUp && !hasLeft) 
+                        ans++; // head of a ship
+                }   
+            }
+        }
+        return ans;
+    }
+
+    // dry run
+    // [["X",".",".","X"],
+    //  [".",".",".","X"],
+    //  [".",".",".","X"]]
+
+
+
+};
