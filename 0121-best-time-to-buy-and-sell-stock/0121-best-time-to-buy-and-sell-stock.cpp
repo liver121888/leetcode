27422a62ab6bc2f7c9cc5fb1af16162f7b805f61
Li-Wei Yang <68542467+liver121888@users.noreply.github.com>
2026-02-09 15:36:25 -0800

    Time: 0 ms (100%), Space: 97.4 MB (59.86%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,19 +1,35 @@
+// class Solution {
+// public:
+//     int maxProfit(vector<int>& prices) {
+
+//         // min must before the max
+//         // from the back we find max
+//         // from the font we find min and cal the different
+
+//         // curMaX and curMin
+//         int maxProfit = 0; int curMin = INT_MAX;
+//         for (auto price : prices) {
+//             if (price < curMin)
+//                 curMin = price;
+//             else if (price - curMin > maxProfit)
+//                 maxProfit = price - curMin;
+//         }
+//         return maxProfit;
+//     }
+// };
+
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
 
-        // min must before the max
-        // from the back we find max
-        // from the font we find min and cal the different
-
-        // curMaX and curMin
-        int maxProfit = 0; int curMin = INT_MAX;
-        for (auto price : prices) {
-            if (price < curMin)
-                curMin = price;
-            else if (price - curMin > maxProfit)
-                maxProfit = price - curMin;
+        int maxProfit = 0;
+        int currMinPrice = INT_MAX;
+        for (int i = 0; i < prices.size(); i++) {
+            if (prices[i] < currMinPrice)
+                currMinPrice = prices[i];
+            maxProfit = max(maxProfit, prices[i] - currMinPrice);
         }
+
         return maxProfit;
     }
 };
