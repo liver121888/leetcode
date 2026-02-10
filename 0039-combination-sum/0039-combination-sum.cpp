0adbf10737cdf5110268217e0748ef03cb3a0c6d
Li-Wei Yang <68542467+liver121888@users.noreply.github.com>
2026-02-10 15:28:24 -0800

    Time: 1129 ms (5.11%), Space: 369.4 MB (5.74%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,48 @@
+// we need to return the list of possible combination 
+// that sums up to target
+// we can do dp 
+// let target is k and candidates.size = n
+// in each position, we branch out n possibilities
+// the depth in wores case is k
+// time: O(n^k)
+
+// if we dp
+// O(k)
+// space: O(k)
+
+// we also need to return the possible combination
+// we use backtracking and remember to restore state
+
+class Solution {
+public:
+
+    void dfs(const vector<int>& candidates, vector<vector<int>>& ans, vector<int> currState, int target) {
+
+        if (target == 0) {
+            sort(currState.begin(),currState.end());
+            if (find(ans.begin(), ans.end(), currState) == ans.end())
+                ans.push_back(currState);
+            return;
+        }
+
+        if (target < 0) {
+            return;
+        }
+
+        for (auto& candidate : candidates) {
+            // modify state
+            currState.push_back(candidate);
+            dfs(candidates, ans, currState, target-candidate);
+            // restore state
+            currState.pop_back();
+        }
+
+    }
+
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+
+        vector<vector<int>> ans;
+        dfs(candidates, ans, {}, target);
+        return ans;
+    }
+};
