7db9c5102be5a64bb088aab5e6be9405af03d995
Li-Wei Yang <68542467+liver121888@users.noreply.github.com>
2026-02-09 16:14:40 -0800

    Time: 0 ms (100%), Space: 30.3 MB (37.06%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -1,17 +1,53 @@
+
+// non-decreasing order
+// we can square each of them and sort
+// O(nlogn)
+// we can find the zero and square from the middle
+// and arrange them
+
+// an O(n) solution using a different approach?
+
 class Solution {
 public:
+
+
+    // find the first occurance that a number > 0
+
     vector<int> sortedSquares(vector<int>& nums) {
+
+        // last non-positive
+        // auto it = upperbound(nums.begin(), nums.end(), 0);
+        // int idx = it - nums.begin();
+
+        // we don't need to do the above
+        // we do two pointers
+        // l -> the most negative
+        // r -> the most positive, input position
+
         int n = nums.size();
-        vector<int> ans(n);
-        int r = n - 1;
-        int l = 0;
-        for (int i = n - 1; i >=0; i--) {
-            if (abs(nums[l]) > abs(nums[r])) {
-                ans[i] = nums[l]*nums[l++];
+        vector<int> ans(n, -1);
+
+        int l = 0, r = n - 1, idx = n - 1;
+        while (l <= r) {
+            
+            int lSquare = nums[l] * nums[l];
+            int rSquare = nums[r] * nums[r];
+
+            if (lSquare > rSquare) {
+                l++;
             } else {
-                ans[i] = nums[r]*nums[r--]; 
+                r--;
             }
-        }        
-        return ans;        
+
+            ans[idx] = max(lSquare, rSquare);
+            idx--;
+        }
+
+        // [-4,-1,0,3,10]
+
+        // 100 
+
+
+        return ans;
     }
 };
