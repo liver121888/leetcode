66bb8213b7611bc552aae1917422d07a602866c6
Li-Wei Yang <68542467+liver121888@users.noreply.github.com>
2026-02-12 16:51:08 -0800

    Time: 49 ms (45.19%), Space: 94.1 MB (37.07%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -1,22 +1,44 @@
+// we first sort the array
+// minimum number of intervals you need to remove
+// we want to remove the minimum number
+
+// [[1,2],[2,3],[3,4],[1,3]]
+
+// [[1,2],[1,2],[1,2]]
+
+// [[1,2],[2,3]]
+
+// then check how can we remove minimum number
+// when consider remove [1, 7] and [1, 8]
+// we will want to remove 1, 7
+
+// true means should be place in front
+bool compare(vector<int>& a, vector<int>& b) {
+    return a[1] < b[1];
+}
+
+
 class Solution {
 public:
-    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
-        sort(intervals.begin(), intervals.end());
-        // for (auto x : intervals)
-        //     cout << x[0] << ' | ' << x[1] << endl;
-        int ans = 0;
-        int prevEnd = intervals[0][1];
-        for (int i = 1; i < intervals.size(); i++) {
-            if (intervals[i][0] < prevEnd) {
-                ans++;
-                // don't have to remove
-                // intervals.erase(intervals.begin() + i);
-                prevEnd = min(prevEnd, intervals[i][1]);
-            } else {                
-                prevEnd = intervals[i][1];
-            }            
-        }
 
+
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+
+        sort(intervals.begin(), intervals.end(), compare);
+
+        int ans = 0;
+        int currBack = INT_MIN;
+        for (int i = 0; i < intervals.size(); i++) {
+
+            // overlap
+            if (intervals[i][0] < currBack) {
+                ans++;
+                // cout << i << " " << ans << " " << currBack << endl;   
+            } else {
+                currBack = intervals[i][1];
+            }
+        }
         return ans;
+        
     }
 };
