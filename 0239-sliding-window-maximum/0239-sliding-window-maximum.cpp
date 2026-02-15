643ab72535d7974e835640eb12e383521b2fe3e7
Li-Wei Yang <68542467+liver121888@users.noreply.github.com>
2026-02-15 10:49:33 -0800

    Time: 18 ms (78.06%), Space: 139.1 MB (88.34%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,21 +1,35 @@
+// O(nk), build sliding windows and take max everytime
+// O(nlogn), we have a heap and swipe through, everytime we look at 
+// the top, and record the pointer to pop
+// O(n)? each step we add one and remove one
+
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        int n = nums.size();
+
         deque<int> dq;
         vector<int> ans;
         
-        for (int i = 0; i < n; i++) {
-            if (!dq.empty() && dq.front() == i - k) 
-                dq.pop_front();
-            while (!dq.empty() && nums[dq.back()] < nums[i])
+        for (int i = 0; i < k; i++) {
+            // this is to remain monotonic, pop all the things that is 
+            // smaller than the current number
+            while (!dq.empty() && nums[i] >= nums[dq.back()])
                 dq.pop_back();
             dq.push_back(i);
-            
-            if (i >= k - 1) 
-                ans.push_back(nums[dq.front()]);
         }
 
-        return ans;  
+        ans.push_back(nums[dq.front()]);
+
+        for (int i = k; i < nums.size(); i++) {
+            if (dq.front() == i - k)
+                dq.pop_front();
+
+            while (!dq.empty() && nums[i] >= nums[dq.back()])
+                dq.pop_back();
+
+            dq.push_back(i);
+            ans.push_back(nums[dq.front()]);
+        }
+        return ans;
     }
 };
