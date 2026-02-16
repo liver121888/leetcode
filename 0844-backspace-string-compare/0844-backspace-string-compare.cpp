8d0e6fd9426e074372cd2a5399e237ec56ae3b2f
Li-Wei Yang <68542467+liver121888@users.noreply.github.com>
2026-02-16 15:45:53 -0800

    Time: 0 ms (100%), Space: 8.3 MB (95.95%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.cpp
+++ 0844-backspace-string-compare/0844-backspace-string-compare.cpp
@@ -8,36 +8,91 @@
 class Solution {
 public:
 
-    string process(string input) {
+    // string process(string input) {
 
-        stack<char> st;
+    //     stack<char> st;
 
-        for (auto c : input) {
+    //     for (auto c : input) {
 
-            if (c == '#') {
-                if (!st.empty()) {
-                    st.pop();
-                }
-                continue;
+    //         if (c == '#') {
+    //             if (!st.empty()) {
+    //                 st.pop();
+    //             }
+    //             continue;
+    //         }
+
+    //         st.push(c);
+    //     }
+
+    //     string res = "";
+    //     while (!st.empty()) {
+    //         res += st.top();
+    //         st.pop();
+    //     }
+
+    //     return res;
+    // }
+
+    int nextValidIndex(const string& s, int i) {
+        int skip = 0;
+        while (i >= 0) {
+            if (s[i] == '#') {
+                skip++;
+                i--;
+            } else if (skip > 0) {
+                skip--;
+                i--;
+            } else {
+                break; // s[i] is a valid character to compare
             }
-
-            st.push(c);
         }
-
-        string res = "";
-        while (!st.empty()) {
-            res += st.top();
-            st.pop();
-        }
-
-        return res;
+        return i;
     }
+
 
     bool backspaceCompare(string s, string t) {
 
-        string ns = process(s);
-        string nt = process(t);
+        // string ns = process(s);
+        // string nt = process(t);
 
-        return ns == nt;
+        // return ns == nt;
+
+        // two pointers
+
+        // "a#c", t = "b"
+
+        // s = 2
+        // t = 0
+
+        // "ab#c", t = "ad#c"
+        // sPtr = 3
+        // tPtr = 3
+        // 2, 2
+        // sPtr = 1, sBCnt = 1
+        // tPtr = 1, tBCnt = 1
+
+        // 0, 0
+
+        int i = (int)s.size() - 1;
+        int j = (int)t.size() - 1;
+
+        while (i >= 0 || j >= 0) {
+            i = nextValidIndex(s, i);
+            j = nextValidIndex(t, j);
+
+            // both finished
+            if (i < 0 && j < 0) return true;
+            // one finished, the other not
+            if (i < 0 || j < 0) return false;
+
+            if (s[i] != t[j]) return false;
+
+            i--;
+            j--;
+        }
+        return true;
+
+
+
     }
 };
