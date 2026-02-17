81f3cef29f1724f4c4c8d01eab6b9b385276da3c
Li-Wei Yang <68542467+liver121888@users.noreply.github.com>
2026-02-16 20:29:00 -0800

    Time: 55 ms (5.49%), Space: 19.3 MB (5.11%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -1,25 +1,51 @@
+// we use stack to record the operator and operand
+// when we met an operand, we pop two and calculate the result and push it back
+
 class Solution {
 public:
     int evalRPN(vector<string>& tokens) {
-        stack<int> st;
-        for (auto s : tokens) {
-            if (s == "+" || s == "-") {
-                int second = st.top();
-                st.pop();
-                int first = st.top();
-                st.pop();
-                st.push(s == "+" ? first + second : first - second);
-            } else if (s == "*" || s == "/") {
-                int second = st.top();
-                st.pop();
-                int first = st.top();
-                st.pop();
-                st.push(s == "*" ? first * second : first / second);
+
+
+        stack<string> st;
+
+        for (const auto& token : tokens) {
+
+            // token is an operator
+            // we pop and calculate
+
+            // token is an operand
+            // we push
+
+            if (!(token == "+" || token == "-" 
+                || token == "*" || token == "/")) {
                 
-            } else
-                st.push(stoi(s));
-            
+                st.push(token);
+
+            } else {
+                string op1Str = st.top();
+                st.pop();
+                string op2Str = st.top();
+                st.pop();
+
+                int op1 = stoi(op1Str);
+                int op2 = stoi(op2Str);
+
+                cout << op1 << " " << op2 << endl;
+
+                if (token == "+") {
+                    st.push(to_string(op2+op1));
+                } else if (token == "-") {
+                    st.push(to_string(op2-op1));
+                } else if (token == "*") {
+                    st.push(to_string(op2*op1));
+                } else {
+                    st.push(to_string(op2/op1));
+
+                }
+            }
+
         }
-        return st.top();
+
+        return stoi(st.top());
     }
 };
