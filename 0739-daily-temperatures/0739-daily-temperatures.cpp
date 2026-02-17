11df5aa0b27f7dd149cd29b2598dba7c51477468
Li-Wei Yang <68542467+liver121888@users.noreply.github.com>
2026-02-16 21:30:49 -0800

    Time: 32 ms (19.38%), Space: 112 MB (8.16%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -1,25 +1,58 @@
+// we need to solve for each idx
+// if we can also record idx as a pair
+// we can solve it as i - j
+// have a stack
+// check top
+// if curr > top
+// idx
+// if curr < top
+// push
+// 73
+// 74
+// we solve and put 74
+// 75 when we find 71
+// push
+// 75 71 69 72
+// solve for 71 69
+// we need to keep idx so we can get the correct number
+
 class Solution {
 public:
+
+    // 73,0 
+    // 74,1
+    // 75,2
+    // 71.3
+    // 69,4
+    // 72,5
+
+
     vector<int> dailyTemperatures(vector<int>& temperatures) {
         
-        int n = temperatures.size();
-        
         stack<pair<int, int>> st;
-        vector<int> ans(n);
-        
+        int n = temperatures.size();
+        vector<int> ans(n, 0);
         for (int i = 0; i < n; i++) {
+            
+            // if not empty, we need to comapre and push
+            // if empty, we push
 
-            while (!st.empty() && st.top().second < temperatures[i]) {
-                int prevDay = st.top().first;
-                int prevTemp = st.top().second;
-                st.pop(); 
-                ans[prevDay] = i - prevDay;
+            int currTemp = temperatures[i];
+            while (!st.empty()) {
+                const auto& [temp, idx] = st.top();
+                if (temp < currTemp) {
+                    ans[idx] = i - idx;
+                } else {
+                    break;
+                }
+                st.pop();
             }
-            st.push({i, temperatures[i]});                
+
+            st.push(make_pair(temperatures[i], i));
 
         }
- 
+
+
         return ans;
-        
     }
 };
