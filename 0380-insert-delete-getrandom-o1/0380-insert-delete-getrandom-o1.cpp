74503d57d76741248af6b8a01357e50c2b77cbda
Li-Wei Yang <68542467+liver121888@users.noreply.github.com>
2026-02-17 17:06:53 -0800

    Time: 47 ms (52.58%), Space: 113.3 MB (57.99%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.cpp
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.cpp
@@ -1,33 +1,55 @@
 class RandomizedSet {
 public:
-    set<int> aSet;
-    RandomizedSet() {
 
+    // number, idx
+    unordered_map<int, int> rMap;
+
+    // idx, number
+    vector<int> v;
+
+    RandomizedSet() {
+        
     }
     
     bool insert(int val) {
-        if (aSet.find(val) != aSet.end()) {
-            return false;            
-        } 
-        aSet.insert(val);
-        return true;
+
+        if (rMap.find(val) == rMap.end()) {
+            v.push_back(val);
+            rMap.insert({val, v.size()-1});
+            return true;
+        }
+        return false;
     }
     
     bool remove(int val) {
-        if (aSet.find(val) != aSet.end()) {
-            aSet.erase(val);
-            return true;            
+        
+        if (rMap.find(val) != rMap.end()) {
+
+            // 刪除技巧：swap with last
+            // 太鬼了
+            // 要刪 arr[idx]
+            // 把最後一個元素搬到 idx
+            // pop_back
+            // 更新 map 裡那個被搬過來的元素的新位置
+
+            // move first
+            int idx = rMap.at(val);
+            int temp = v[v.size()-1];
+            swap(v[idx], v[v.size()-1]);
+            rMap[temp]=idx;
+
+            // then remove
+            rMap.erase(val);
+            v.pop_back();
+            return true;
         }
         return false;
     }
     
     int getRandom() {
-        int n = rand() % aSet.size();
-        auto it = aSet.begin();
-        while(n--){
-            it++;
-        }
-        return *it;
+
+        return v[rand() % v.size()];
+
     }
 };
 
