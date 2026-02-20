a6fd316ef8bf36822a9be8873068cc06384d0d0a
Li-Wei Yang <68542467+liver121888@users.noreply.github.com>
2026-02-19 22:27:25 -0800

    Time: 447 ms (47.33%), Space: 98.6 MB (55.33%) - LeetHub

--- 0317-shortest-distance-from-all-buildings/0317-shortest-distance-from-all-buildings.cpp
+++ 0317-shortest-distance-from-all-buildings/0317-shortest-distance-from-all-buildings.cpp
@@ -1,205 +1,107 @@
-// class Solution {
-// public:
+// brute-force
+// try every zero, we compare the total distance, and find the minimum
+// O(m*n * m * n)
 
-//     // calc heuristic so we can rule out most of the possible solutions
+// [1, 1, 0, 1] -> -1;
+// calculate nearest 1
+// we can start flooding from evey 1
+// we end the algorithm when a 0 is visited by flooding from every one
+// we take out the sum -> minimum dist from all building
+// O(m^2n^2)
+// O(m^2n^2) -> visited times + the sum of dist
 
-//     // int dist(int x0, int y0, int x1, int y1){
-//     //     return abs(x0 - x1) + abs(y0 - y1);
-//     // }
-
-//     // string hash(int i, int j) {
-//     //     return i.to_string() + " " j.to_string();
-//     // }
-
-//     // pair<int, int> unHash(string hash) {
-//     //     int idx = hash.find(' ') - hash.begin();
-//     //     int x = stoi(hash.substring(0, idx));
-//     //     int y = stoi(hash.substring(idx + 1, hash.length()));
-//     //     return make_pair<int, int>(x, y);
-//     // }
-
-//     const vector<vector<int>> dirs = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
-//     int shortestDistance(vector<vector<int>>& grid) {
-
-//         // get all the buildings first
-//         int m = grid.size();
-//         int n = grid[0].size();
-//         int ans = INT_MAX;
-
-//         int totalBuildings = 0;
-//         for (int i = 0; i < m; i++)
-//             for (int j = 0; j < n; j++)
-//                 if (grid[i][j] == 1)
-//                     totalBuildings++;
-
-//         cout << "totalBuildings: " << totalBuildings << endl;
-
-//         // unordered_set<string> buildings;
-//         // O(m*n*m*n) -> O(m^2 * n^2)
-//         // space: O(m*n)
-
-//         for (int i = 0; i < m; i++) {
-//             for (int j = 0; j < n; j++) {
-//                 cout << "i: " << i << " j: " << j << endl;
-//                 if (grid[i][j] == 0) {
-//                     queue<int> bfs;
-//                     unordered_set<int> visited;
-//                     int level = 0;
-//                     int totalTravelDistance = 0;
-//                     int buildingCnts = 0;
-//                     bfs.push(i * n + j);
-//                     bool earlyEnd = false;
-
-//                     while(!bfs.empty()) {
-//                         int currLayerSize = bfs.size();
-//                         while (currLayerSize > 0) {
-//                             auto curr = bfs.front();
-//                             bfs.pop();
-//                             int c = curr % n;
-//                             int r = curr / n;
-
-//                             visited.insert(curr);
-
-//                             for (auto dir : dirs) {
-//                                 int new_r = r + dir[0];
-//                                 int new_c = c + dir[1];
-//                                 // check boundary
-//                                 if (new_r >= m || new_c >= n || new_r < 0 || new_c < 0)
-//                                     continue;
-
-//                                 int neighbor = new_r * n + new_c;
-//                                 if (!visited.count(neighbor)) {
-//                                     int type = grid[new_r][new_c];
-//                                     if (type == 0) {
-//                                         bfs.push(neighbor);
-//                                     } else if (type == 1) {
-//                                         totalTravelDistance += level + 1;
-//                                         buildingCnts++;
-//                                         // for earlyEnd
-//                                         if (buildingCnts == totalBuildings) {
-//                                             earlyEnd = true;
-//                                             break;
-//                                         }
-//                                     }
-//                                     // obstacle otherwise
-//                                 }
-
-//                             }
-//                             if (earlyEnd)
-//                                 break;
-//                             currLayerSize--;
-//                         }
-//                         if (earlyEnd)
-//                             break;
-//                         level++;
-//                     } 
-//                     ans = min(totalTravelDistance, ans);
-//                 }
-//             }
-//         }
-//         return ans;
-//     }
-// };
+// we use 0 as the building, dist in the grid
+// we use -1 as obstaces
+// queue<pair<int,int>> the future flood coords
+// grid[i][j] = numVisit, sum of dist
+// directions vector
+// while (!queue.empty())
 
 class Solution {
-private:
-    int bfs(vector<vector<int>>& grid, int row, int col, int totalHouses) {
-        // Next four directions.
-        int dirs[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-        
-        int rows = grid.size();
-        int cols = grid[0].size();
-        int distanceSum = 0;
-        int housesReached = 0;
+public:
 
-        // Queue to do a bfs, starting from (r,c) cell
-        queue<pair<int, int>> q;
-        q.push({ row, col });
+    int dirs[4][2] = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+    int m;
+    int n;
 
-        // Keep track of visited cells
-        vector<vector<bool>> vis(rows, vector<bool> (cols, false));
-        vis[row][col] = true;
+    void bfs(const vector<vector<int>>& grid, vector<vector<vector<int>>>& data, int y, int x) {
 
-        int steps = 0;
+        vector<vector<bool>> visited(m, vector<bool>(n, false));
+        queue<pair<int,int>> q;
+        q.push({y, x});
+        visited[y][x] = true;
 
-        while (!q.empty() && housesReached != totalHouses) {
-            for (int i = q.size(); i > 0; --i) {
-                auto curr = q.front();
+        int level = 0;
+
+        while (!q.empty()) {
+
+            int numNode = q.size();
+            while (numNode) {
+                auto [r, c] = q.front();
                 q.pop();
 
-                row = curr.first;
-                col = curr.second;
-
-                // If this cell is a house, then add the distance from the source to this cell
-                // and we go past from this cell.
-                if (grid[row][col] == 1) {
-                    distanceSum += steps;
-                    housesReached++;
-                    continue;
+                // If we reached an empty cell, then add the distance
+                // and increment the count of houses reached at this cell.
+                if (grid[r][c] == 0) {
+                    data[r][c][1] += level;
+                    data[r][c][0] += 1;
                 }
 
-                // This cell was an empty cell, hence traverse the next cells which is not a blockage.
-                for (auto& dir : dirs) {
-                    int nextRow = row + dir[0];
-                    int nextCol = col + dir[1];
+                for (const auto& dir : dirs)  {
+                    int nr = r + dir[0];
+                    int nc = c + dir[1];
 
-                    if (nextRow >= 0 && nextCol >= 0 && nextRow < rows && nextCol < cols) {
-                        if (!vis[nextRow][nextCol] && grid[nextRow][nextCol] != 2) {
-                            vis[nextRow][nextCol] = true;
-                            q.push({nextRow, nextCol});
-                        }
+                    if (nr >= m || nc >= n || nr < 0 || nc < 0)
+                        continue;
+
+                    if (!visited[nr][nc] && grid[nr][nc] == 0) {
+                        // data[nr][nc][0]++;
+                        // data[nr][nc][1] += level;
+                        visited[nr][nc] = true;
+                        q.push({nr, nc});
                     }
                 }
+
+                numNode--;
             }
-            
-            // After traversing one level cells, increment the steps by 1 to reach to next level.
-            steps++;
+
+            level++;
         }
 
-        // If we did not reach all houses, then any cell visited also cannot reach all houses.
-        // Set all cells visted to 2 so we do not check them again and return INT_MAX.
-        if (housesReached != totalHouses) {
-            for (row = 0; row < rows; row++) {
-                for (col = 0; col < cols; col++) {
-                    if (grid[row][col] == 0 && vis[row][col]) {
-                        grid[row][col] = 2;
-                    }
-                }
-            }
-            return INT_MAX;
-        }
-        // If we have reached all houses then return the total distance calculated.
-        return distanceSum;
     }
 
-public:
     int shortestDistance(vector<vector<int>>& grid) {
-        int minDistance = INT_MAX;
-        int rows = grid.size();
-        int cols = grid[0].size();
-        int totalHouses = 0;
 
-        for (int row = 0; row < rows; ++row) {
-            for (int col = 0; col < cols; ++col) {
-                if (grid[row][col] == 1) { 
-                    totalHouses++;
+        m = grid.size();
+        n = grid[0].size();
+
+        // number of visits, sum of distance
+        vector<vector<vector<int>>> data(m, vector<vector<int>>(n, {0, 0})); 
+
+        int houseNum = 0;
+
+        // traverse to find 1
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    houseNum++;
+                    bfs(grid, data, i, j);
                 }
             }
         }
 
-        // Find the min distance sum for each empty cell.
-        for (int row = 0; row < rows; ++row) {
-            for (int col = 0; col < cols; ++col) {
-                if (grid[row][col] == 0) {
-                    minDistance = min(minDistance, bfs(grid, row, col, totalHouses));
+        int minDistSum = INT_MAX;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int numVisit = data[i][j][0];
+                int distSum = data[i][j][1];
+                if (numVisit == houseNum) {
+                    minDistSum = min(distSum, minDistSum);
                 }
-            }
-        }
 
-        // If it is impossible to reach all houses from any empty cell, then return -1.
-        if (minDistance == INT_MAX) {
-            return -1;
+            }
+
         }
-        return minDistance;
+        return minDistSum == INT_MAX ? -1 : minDistSum;
     }
 };
