ac8e01a4d326898731b7e13e6c451a9d35a26af6
Li-Wei Yang <68542467+liver121888@users.noreply.github.com>
2026-02-25 14:11:19 -0800

    Time: 0 ms (100%), Space: 23 MB (82.23%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -17,30 +17,87 @@
 
 // we can solve this using preorder and compare the left dpeth and the right depth
 
+// 核心思想
+// 不要：
+// 先算高度，再檢查平衡
+// 改成：
+// 在算高度的同時，就檢查是否平衡
+// bottom-up
 class Solution {
 public:
+    int dfs(TreeNode* root) {
+        if (!root) return 0;
 
-    bool ans = true;
+        int left = dfs(root->left);
+        if (left == -1) return -1;
 
-    int getDepth(TreeNode* root, int depth) {
+        int right = dfs(root->right);
+        if (right == -1) return -1;
 
-        if (!root)
+        if (abs(left - right) > 1)
             return -1;
 
-        int leftDepth = getDepth(root->left, depth+1);
-        leftDepth = leftDepth == -1 ? depth : leftDepth;
-        int rightDepth = getDepth(root->right, depth+1);
-        rightDepth = rightDepth == -1 ? depth : rightDepth;
-
-        if (abs(leftDepth - rightDepth) > 1)
-            ans = false;
-        return max(leftDepth, rightDepth);
+        return 1 + max(left, right);
     }
-
 
     bool isBalanced(TreeNode* root) {
-        int depth = getDepth(root, 0);
-        return ans;
-
+        return dfs(root) != -1;
     }
 };
+
+
+// top-down
+// class Solution {
+// private:
+//     // Recursively obtain the height of a tree. An empty tree has -1 height
+//     int height(TreeNode* root) {
+//         // An empty tree has height -1
+//         if (root == NULL) {
+//             return -1;
+//         }
+//         return 1 + max(height(root->left), height(root->right));
+//     }
+
+// public:
+//     bool isBalanced(TreeNode* root) {
+//         // An empty tree satisfies the definition of a balanced tree
+//         if (root == NULL) {
+//             return true;
+//         }
+
+//         // Check if subtrees have height within 1. If they do, check if the
+//         // subtrees are balanced
+//         return abs(height(root->left) - height(root->right)) < 2 &&
+//                isBalanced(root->left) && isBalanced(root->right);
+//     }
+// };
+
+
+// bottom-up 我自己的寫法
+// class Solution {
+// public:
+
+//     bool ans = true;
+
+//     int getDepth(TreeNode* root, int depth) {
+
+//         if (!root)
+//             return -1;
+
+//         int leftDepth = getDepth(root->left, depth+1);
+//         leftDepth = leftDepth == -1 ? depth : leftDepth;
+//         int rightDepth = getDepth(root->right, depth+1);
+//         rightDepth = rightDepth == -1 ? depth : rightDepth;
+
+//         if (abs(leftDepth - rightDepth) > 1)
+//             ans = false;
+//         return max(leftDepth, rightDepth);
+//     }
+
+
+//     bool isBalanced(TreeNode* root) {
+//         int depth = getDepth(root, 0);
+//         return ans;
+
+//     }
+// };
