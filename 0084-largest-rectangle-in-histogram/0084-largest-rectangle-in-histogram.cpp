773324d78cd210f7c42141521e4e74803c7eb177
Li-Wei Yang <68542467+liver121888@users.noreply.github.com>
2026-02-26 14:41:41 -0800

    Time: 31 ms (28.06%), Space: 79.4 MB (94.34%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -1,4 +1,4 @@
-//  return the area of the largest rectangle in the histogram.
+// return the area of the largest rectangle in the histogram.
 // Input: heights = [2,1,5,6,2,3]
 // Output: 10
 // because 6 > 5, so we can have 2 * 5 = 10
@@ -7,16 +7,6 @@
 // we are finding
 // a subarray that max(min(subarray_elements) * (r - l +1))
 // brute-force every subarray and calculate the max area
-
-// Container With Most Water -> we solved using two pointers
-
-// 1 <= heights.length <= 10^5
-// 0 <= heights[i] <= 10^4
-
-// Monotonic Stack
-// trapping rain water
-// it can only be larger
-// 2
 
 class Solution {
 public:
@@ -45,8 +35,10 @@ public:
         int max_area = 0;
         for (int i = 0; i < heights.size(); i++) {
             
-            // 新來的histogram沒有比較高的時候
+            // 新來的histogram沒有比較高的時候，可結算
             while (st.top() != -1 && heights[st.top()] >= heights[i]) {
+                // current height: 如果current柱子當作最矮的話
+                // 那我的界就是在左右邊比我更矮的
                 int current_height = heights[st.top()];
                 st.pop();
                 int current_width = i - st.top() - 1;
@@ -55,6 +47,7 @@ public:
             st.push(i);
         }
 
+        // 最後再算一次確保算完
         while (st.top() != -1) {
             int current_height = heights[st.top()];
             st.pop();
