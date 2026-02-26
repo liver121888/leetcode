3b36d74c9fd956e1135d67eb3c37fd9d8f7f2932
Li-Wei Yang <68542467+liver121888@users.noreply.github.com>
2026-02-25 22:16:26 -0800

    Time: 117 ms (36.18%), Space: 49.1 MB (5.56%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -1,7 +1,36 @@
 
-// we need shortest path
-// valid edge is only differ by a single letter
-// we can model this problem as a graph problem
+
+// Input: beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log","cog"]
+// "hit" -> "hot" -> "dot" -> "dog" -> "cog"
+// Output: 5
+
+// Input: beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log"]
+// Output: 0
+
+// if the endword is not in the wordlist
+// return 0
+
+// "1" "-"
+// lowercase character
+
+// hot <-> dot build a edge
+
+// multiple ways to start the bfs "hit"
+// wordList = ["hot","hic", "dot","dog","lot","log","cog"]
+// for the valid start position, we bfs and see if we can find the target word
+// wordList is of size n
+// time: O(n) we marked visited, visit a node one time
+// space: O(n^2)
+
+// hot, we need to quickly find all the word that it can transition to
+// hot [dot hic]
+// how can we do this efficiently
+// hot -> *ot, h*t, ho*
+// dot -> *ot, d*t, do*
+// hic -> *ic, h*c, hi*
+
+// *ot, {hot, dot}, h*t
+// we traverse this map to build edges between the words
 
 
 class Solution {
@@ -9,37 +38,26 @@ public:
 
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
 
-        // beginWord does not need to be in wordList
-        // 1. it's in the word list, then we just start with that and find optimal path
-        // 2. it's not, we need to find all possible starting point in the graph
-        // all nodes are unique
-        // wordList[i].length == beginWord.length
-        
-        // sort(wordList.begin(), wordList.end());
-        // ["hot","dot","dog","lot","log","cog"]
-        // ["cog", "dog", "dot", hot, log, lot]
-
-
-        // Since all words are of same length.
-        int L = beginWord.size();
-        // Dictionary to hold combination of words that can be formed,
-        // from any given word. By changing one letter at a time.
-        unordered_map<string, vector<string>> allComboDict;
-        for (string word : wordList) {
-            for (int i = 0; i < L; i++) {
-                // Key is the generic word
-                // Value is a list of words which have the same intermediate
-                // generic word.
-                string newWord =
-                    word.substr(0, i) + '*' + word.substr(i + 1, L);
-                allComboDict[newWord].push_back(word);
+        int strLen = wordList[0].length();
+        // key, e.g. *ot, h*t, value is the index
+        unordered_map<string, vector<string>> groups;
+        bool hasSolution = false;
+        for (int i = 0; i < wordList.size(); i++) {
+            string word = wordList[i];
+            if (word == endWord)
+                hasSolution = true;
+            for (int j = 0; j < strLen; j++) {
+                // substr自動取到結尾
+                string key = word.substr(0, j) + '*' + word.substr(j+1);
+                groups[key].push_back(word);
             }
         }
+        if (!hasSolution)
+            return 0;
 
         // Queue for BFS
         queue<pair<string, int>> Q;
         Q.push(make_pair(beginWord, 1));
-        // Visited to make sure we don't repeat processing same word.
         unordered_map<string, bool> visited;
         visited[beginWord] = true;
         while (!Q.empty()) {
@@ -47,13 +65,12 @@ public:
             Q.pop();
             string word = node.first;
             int level = node.second;
-            for (int i = 0; i < L; i++) {
+            for (int i = 0; i < strLen; i++) {
                 // Intermediate words for current word
-                string newWord =
-                    word.substr(0, i) + '*' + word.substr(i + 1, L);
+                string key = word.substr(0, i) + '*' + word.substr(i + 1);
                 // Next states are all the words which share the same
                 // intermediate state.
-                for (string adjacentWord : allComboDict[newWord]) {
+                for (string adjacentWord : groups[key]) {
                     // If at any point if we find what we are looking for
                     // i.e. the end word - we can return with the answer.
                     if (adjacentWord == endWord) {
@@ -65,8 +82,15 @@ public:
                         Q.push(make_pair(adjacentWord, level + 1));
                     }
                 }
+
+
             }
+
         }
+
+
+
         return 0;
+        
     }
 };
