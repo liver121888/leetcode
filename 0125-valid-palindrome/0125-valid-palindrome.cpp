e8360844a5788f2816837d89392af32bb8b2ca9c
Li-Wei Yang <68542467+liver121888@users.noreply.github.com>
2026-03-02 13:30:13 -0800

    Time: 0 ms (100%), Space: 9.9 MB (77.5%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,34 +1,49 @@
+// class Solution {
+// public:
+//     bool isPalindrome(string s) {
+
+//         // two pointers
+//         // we skip anny speical character
+        
+//         // "A man, a plan, a canal: Panama"
+
+//         int l = 0, r = s.length() - 1;
+
+//         while (l < r) {
+
+//             if (!isalnum(s[l])) {
+//                 l++;
+//                 continue;
+//             }
+
+//             if (!isalnum(s[r])) {
+//                 r--;
+//                 continue;
+//             }
+
+//             if (tolower(s[l]) != tolower(s[r]))
+//                 return false;
+//             else {
+//                 l++;
+//                 r--;
+//             }
+
+//         }
+//         return true;
+//     }
+// };
+
+// concise solution
 class Solution {
 public:
     bool isPalindrome(string s) {
+        for (int i = 0, j = s.size() - 1; i < j; i++, j--) {
+            while (i < j && !isalnum(s[i])) i++;
+            while (i < j && !isalnum(s[j])) j--;
 
-        // two pointers
-        // we skip anny speical character
-        
-        // "A man, a plan, a canal: Panama"
-
-        int l = 0, r = s.length() - 1;
-
-        while (l < r) {
-
-            if (!isalnum(s[l])) {
-                l++;
-                continue;
-            }
-
-            if (!isalnum(s[r])) {
-                r--;
-                continue;
-            }
-
-            if (tolower(s[l]) != tolower(s[r]))
-                return false;
-            else {
-                l++;
-                r--;
-            }
-
+            if (tolower(s[i]) != tolower(s[j])) return false;
         }
+
         return true;
     }
 };
