fab5e5aeb6f28c3d3617ba7df1454864e0ab3aea
Li-Wei Yang <68542467+liver121888@users.noreply.github.com>
2026-03-03 09:29:54 -0800

    Time: 0 ms (100%), Space: 71.7 MB (80.23%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
 
 
-    // why we cannot use two pointers
+    // why we cannot use sliding window
     // it has negative numbers so expanding not resulting larger value
     // we can use dp
     // expanding from one array and for each position the dp stores the ans 
@@ -24,17 +24,33 @@ public:
     // 1, we compare previous largest and 1 and previous largest + 1
     // 2, we comopare previous largest and adding the current or not
 
-
     int maxSubArray(vector<int>& nums) {
         int n = nums.size();
         // base case
         int maxSum = nums[0];
         int currSum = 0;
         for (int i = 0; i < n; i++) {
+            // If current_subarray is negative, throw it away. Otherwise, keep
+            // adding to it.
             currSum = max(0, currSum) + nums[i];
             maxSum = max(currSum, maxSum);
         }
         return maxSum;
         
     }
+
+    // int maxSubArray(vector<int>& nums) {
+    //     // Initialize our variables using the first element.
+    //     int currentSubarray = nums[0];
+    //     int maxSubarray = nums[0];
+    //     // Start with the 2nd element since we already used the first one.
+    //     for (int i = 1; i < nums.size(); i++) {
+    //         // If current_subarray is negative, throw it away. Otherwise, keep
+    //         // adding to it.
+    //         currentSubarray = max(nums[i], currentSubarray + nums[i]);
+    //         maxSubarray = max(maxSubarray, currentSubarray);
+    //     }
+    //     return maxSubarray;
+    // }
+
 };
