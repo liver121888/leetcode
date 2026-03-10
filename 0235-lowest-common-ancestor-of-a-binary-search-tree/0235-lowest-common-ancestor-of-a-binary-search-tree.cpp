285544ea63e7b9d8245aa858eb6fb05163e5f7de
Li-Wei Yang <68542467+liver121888@users.noreply.github.com>
2026-03-10 14:02:22 -0700

    Time: 21 ms (72.05%), Space: 23.6 MB (34.31%) - LeetHub

---
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+// find LCA, post order traversal
+// but a bst
+// we have two nodes we need to find
+// so when the curr root > p root > q
+// we need to search the left part
+// if root < p root > q
+
+// when we dfs find the node the first one is goann be the lowerest
+// 
+
+class Solution {
+public:
+
+    TreeNode* ans;
+
+
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        
+        int parentVal = root->val;
+
+        // Value of p
+        int pVal = p->val;
+
+        // Value of q;
+        int qVal = q->val;
+
+        if (pVal > parentVal && qVal > parentVal) {
+            return lowestCommonAncestor(root->right, p, q);
+        } else if (pVal < parentVal && qVal < parentVal) {
+            return lowestCommonAncestor(root->left, p, q);
+        } else { 
+             // We have found the split point, i.e. the LCA node.
+            return root;
+        }
+    }
+};
