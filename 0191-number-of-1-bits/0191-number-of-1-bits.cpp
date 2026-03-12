d250a9bb52d0cb54c103bc9c649cfefa7ac65ae4
Li-Wei Yang <68542467+liver121888@users.noreply.github.com>
2026-03-12 15:11:01 -0700

    Time: 0 ms (100%), Space: 8.2 MB (81.34%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -3,18 +3,35 @@
 // how would you optimize it?
 class Solution {
 public:
+
+    // can we do O(n)
+    // The run time depends on the number of 1-bits in n. 
+    // In the worst case, all bits in n are 1-bits. 
+    // In case of a 32-bit integer, the run time is O(1).
     int hammingWeight(int n) {
-
-        int ans = 0;
-        while (n) {
-            ans += n & 1;
-            n >>= 1;
-        }
-
-        // hamming distance 
-        // int diff = n ^ 0;
-
-        return ans;
         
+        int count = 0;
+        while (n) {
+            n &= (n - 1);  // Clear the lowest set bit
+            count++;
+        }
+        return count;
     }
+
+
+
+    // O(logn)
+    // int hammingWeight(int n) {
+
+    //     int ans = 0;
+    //     while (n) {
+    //         ans += n & 1;
+    //         n >>= 1;
+    //     }
+
+    //     // hamming distance 
+    //     // int diff = n ^ 0;
+
+    //     return ans;
+    // }
 };
