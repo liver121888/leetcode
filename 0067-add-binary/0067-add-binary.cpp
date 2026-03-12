0a56fae640e182a9ec7a7b46c8d7c1a59bcd2e21
Li-Wei Yang <68542467+liver121888@users.noreply.github.com>
2026-03-12 11:14:23 -0700

    Time: 0 ms (100%), Space: 10.4 MB (17.91%) - LeetHub

--- 0067-add-binary/0067-add-binary.cpp
+++ 0067-add-binary/0067-add-binary.cpp
@@ -6,8 +6,14 @@
 
 class Solution {
 public:
-    string addBinary(string a, string b) {
 
+    // a popular Facebook variation of this problem when the interviewer 
+    // provides you with two numbers and asks to sum them up without 
+    // using the addition operation.
+
+
+
+    string addBinary(string a, string b) {
 
         // start from the least significant bit
         reverse(a.begin(), a.end());
@@ -17,6 +23,12 @@ public:
         int bLen = b.length();
         int longerLen = aLen > bLen ? aLen : bLen;
 
+        // Clever
+        // int n = a.size(), m = b.size();
+        // // Ensure 'a' is always the longer string
+        // if (n < m)
+        //     return addBinary(b, a);
+
         ostringstream out;
         int carry = 0;
         for (int i = 0; i < longerLen; i++) {
@@ -25,7 +37,7 @@ public:
                 sum += a[i] - '0';
             if (i < bLen)
                 sum += b[i] - '0';
-            cout << sum << endl;
+            // cout << sum << endl;
             carry = sum/2;
             out << to_string(sum%2);
         }
@@ -37,3 +49,17 @@ public:
         
     }
 };
+
+// built in
+// class Solution {
+// public:
+//     string addBinary(string a, string b) {
+//         int numA = stoi(a, nullptr, 2);  // Convert binary string to integer
+//         int numB = stoi(b, nullptr, 2);  // Convert binary string to integer
+//         int sum = numA + numB;           // Sum the integers
+//         if (sum == 0) return "0";
+//         string binary =
+//             bitset<32>(sum).to_string();  // Convert sum to binary string
+//         return binary.substr(binary.find('1'));
+//     }
+// };
