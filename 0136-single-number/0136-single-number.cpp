cf0ada2a4cbe3fe8a00d4e61e709aa73f53d23a9
Li-Wei Yang <68542467+liver121888@users.noreply.github.com>
2026-03-12 15:23:48 -0700

    Time: 0 ms (100%), Space: 20.6 MB (83%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        if (nums.size() == 1)
-            return nums[0];        
-        
-        int cur = nums[0];
+
+        int curr = nums[0];
         for (int i = 1; i < nums.size(); i++) {
-            cur = cur ^ nums[i];
+            curr = curr ^ nums[i];
         }
-        return cur;
+        return curr;
     }
 };
