9a396c1a51ea422a255f459e8dcf331ca636e9af
Li-Wei Yang <68542467+liver121888@users.noreply.github.com>
2026-03-12 15:27:29 -0700

    Time: 0 ms (100%), Space: 20.6 MB (58.06%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,10 +1,13 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-
+        // 一樣的設成0
+        // 不一樣的設成1
+        // a ^ 0 = a
+        // a ^ a = 0
         int curr = nums[0];
         for (int i = 1; i < nums.size(); i++) {
-            curr = curr ^ nums[i];
+            curr ^= nums[i];
         }
         return curr;
     }
