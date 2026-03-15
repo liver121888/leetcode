639bad12d47d3aca92db87c7b91e501150565de4
Li-Wei Yang <68542467+liver121888@users.noreply.github.com>
2026-03-15 15:25:23 -0700

    Time: 0 ms (100%), Space: 29.4 MB (7.2%) - LeetHub

---
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.cpp
@@ -0,0 +1,62 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+// we can do like a inorder traversal to look at the tree
+// inorder should be the same of the other inorder
+
+// O(n), traversal
+// O(n), save result of traversal
+
+class Solution {
+public:
+
+    bool sameTree(TreeNode* a, TreeNode* b) {
+
+        if (!a && !b)
+            return true;
+        if (!a || !b)
+            return false;
+
+        if (a->val != b->val)
+            return false;
+
+        return sameTree(a->left, b->left) && sameTree(a->right, b->right);
+    }
+
+
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+
+        queue<TreeNode*> bfs;
+
+        bfs.push(root);
+
+        while(!bfs.empty()) {
+            TreeNode* curr = bfs.front();
+            bfs.pop();
+
+            if (sameTree(curr, subRoot))
+                return true;
+
+            if (curr->left)
+                bfs.push(curr->left);
+
+            if (curr->right)
+                bfs.push(curr->right);
+        }
+
+        return false;        
+    }
+
+
+
+
+};
