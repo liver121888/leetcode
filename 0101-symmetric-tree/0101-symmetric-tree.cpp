dbf190633fefbd208dea2d9fab09e1de77789f35
Li-Wei Yang <68542467+liver121888@users.noreply.github.com>
2026-03-14 22:59:09 -0700

    Time: 0 ms (100%), Space: 18.6 MB (26.38%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.cpp
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -19,26 +19,53 @@ class Solution {
 public:
 
     // recursive
-    bool isMirror(TreeNode* t1, TreeNode* t2) {
-        if (!t1 && !t2)
-            return true;
-        if (!t1 || !t2)
-            return false;
+    // bool isMirror(TreeNode* t1, TreeNode* t2) {
+    //     if (!t1 && !t2)
+    //         return true;
+    //     if (!t1 || !t2)
+    //         return false;
 
-        if (t1->val != t2->val)
-            return false; 
+    //     if (t1->val != t2->val)
+    //         return false; 
 
-        return isMirror(t1->right, t2->left) && isMirror(t1->left, t2->right);
+    //     return isMirror(t1->right, t2->left) && isMirror(t1->left, t2->right);
 
-    }
-    bool isSymmetric(TreeNode* root) {
-        return isMirror(root, root);
-    }
-    
-    // iterative
+    // }
     // bool isSymmetric(TreeNode* root) {
     //     return isMirror(root, root);
     // }
+    
+    // iterative
+    bool isSymmetric(TreeNode* root) {
+
+        queue<TreeNode*> q;
+        q.push(root);
+        q.push(root);
+
+        while (!q.empty()) {
+            TreeNode* leftNode = q.front();
+            q.pop();
+            TreeNode* rightNode = q.front();
+            q.pop();
+
+            if (!leftNode && !rightNode)
+                continue;
+
+            if (!leftNode || !rightNode)
+                return false;
+
+                
+            if (leftNode->val != rightNode->val)
+                return false;
+
+            q.push(leftNode->left);
+            q.push(rightNode->right);
+            q.push(leftNode->right);
+            q.push(rightNode->left);
+
+        }
+        return true;
+    }
 
 
 
