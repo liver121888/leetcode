b540b699a5a8620aea4d9b574e25ce6330c11404
Li-Wei Yang <68542467+liver121888@users.noreply.github.com>
2026-03-15 16:45:13 -0700

    Time: 0 ms (100%), Space: 22 MB (16.31%) - LeetHub

---
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -0,0 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+// A valid BST is defined as follows:
+
+// The left subtree of a node contains only nodes with keys strictly less than the node's key.
+// The right subtree of a node contains only nodes with keys strictly greater than the node's key.
+// Both the left and right subtrees must also be binary search trees.
+ 
+// save result inorder traversal
+// check if it's strictly increasing
+// O(n)
+// O(n)
+class Solution {
+public:
+
+    vector<int> result;
+
+    void inorder(TreeNode* root) {
+
+        if (!root)
+            return;
+
+        inorder(root->left);
+        result.push_back(root->val);
+        inorder(root->right);
+    }
+
+    bool isValidBST(TreeNode* root) {
+
+        inorder(root);
+        for (int i = 1; i < result.size(); i++) {
+
+            if (result[i] <= result[i-1])
+                return false;
+        }
+
+        return true;
+        
+    }
+};
