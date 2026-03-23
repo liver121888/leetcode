ff0c45dedb18fe03103afeec4c088a99f5434f05
Li-Wei Yang <68542467+liver121888@users.noreply.github.com>
2026-03-23 13:20:42 -0700

    Time: 10 ms (60.66%), Space: 14.2 MB (75.12%) - LeetHub

--- 1650-lowest-common-ancestor-of-a-binary-tree-iii/1650-lowest-common-ancestor-of-a-binary-tree-iii.cpp
+++ 1650-lowest-common-ancestor-of-a-binary-tree-iii/1650-lowest-common-ancestor-of-a-binary-tree-iii.cpp
@@ -19,29 +19,24 @@ class Solution {
 public:
 
 
-    Node* LCA(Node* root, Node* p, Node* q) {
+    // Node* LCA(Node* root, Node* p, Node* q) {
 
-        if (!root)
-            return nullptr;
+    //     if (!root)
+    //         return nullptr;
         
-        if (root == p || root == q)
-            return root;
+    //     if (root == p || root == q)
+    //         return root;
 
-        Node* left = LCA(root->left, p, q);
-        Node* right = LCA(root->right, p, q);
+    //     Node* left = LCA(root->left, p, q);
+    //     Node* right = LCA(root->right, p, q);
         
-        if (left && right)
-            return root;
+    //     if (left && right)
+    //         return root;
 
-        return left ? left : right;
+    //     return left ? left : right;
 
-    }
+    // }
 
-    // This solution is mind blowing. The idea is this say p to root is path length 
-    // is a + c and q to root path length is b + c where c is the length of the common 
-    // path to root after they meet. Then we are just making them both travel the same 
-    // distance a + b +c by this swapping when root trick. So they exactly meet at that 
-    // common merging point which is the LCA.
     Node* lowestCommonAncestor(Node* p, Node * q) {
 
         // view it has graph problem
@@ -54,25 +49,29 @@ public:
 
         // or simply find the root then call LCA
         // to have O(1) space
+        // method 1
+        // if (!p || !q)
+        //     return nullptr;
 
-        if (!p || !q)
-            return nullptr;
-
-        Node* curr = p;
-        while (curr && curr->parent) {
-            curr = curr->parent;
-        }
-        return LCA(curr, p, q);
-
-
-
-        // Node* p1 = p, *p2 = q;
-        // while (p1 != p2) {
-        //     p1 = p1->parent ? p1->parent : q;
-        //     p2 = p2->parent ? p2->parent : p;
+        // Node* curr = p;
+        // while (curr && curr->parent) {
+        //     curr = curr->parent;
         // }
+        // return LCA(curr, p, q);
 
-        // return p1;
+
+        // This solution is mind blowing. The idea is this say p to root is path length 
+        // is a + c and q to root path length is b + c where c is the length of the common 
+        // path to root after they meet. Then we are just making them both travel the same 
+        // distance a + b +c by this swapping when root trick. So they exactly meet at that 
+        // common merging point which is the LCA.
+        Node* p1 = p, *p2 = q;
+        while (p1 != p2) {
+            p1 = p1->parent ? p1->parent : q;
+            p2 = p2->parent ? p2->parent : p;
+        }
+
+        return p1;
         
     }
 };
