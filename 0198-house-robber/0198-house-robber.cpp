15a8f9d48c0ad7b5f4e17173f6a05dbf097d832b
Li-Wei Yang <68542467+liver121888@users.noreply.github.com>
2026-03-27 13:18:22 -0700

    Time: 0 ms (100%), Space: 10.1 MB (88.39%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -34,13 +34,24 @@ public:
         if (n == 2)
             return max(nums[0], nums[1]);
 
-        vector<int> dp(n);
-        dp[0] = nums[0];
-        dp[1] = max(nums[0], nums[1]);
+        // vector<int> dp(n);
+        // dp[0] = nums[0];
+        // dp[1] = max(nums[0], nums[1]);
+
+        int firstOption = nums[0];
+        int secondOption = max(nums[0], nums[1]);
+
+        // dp[i] = max(dp[i - 2] + nums[i], dp[i-1]);
+
         for (int i = 2; i < n; i++) {
-            dp[i] = max(dp[i - 2] + nums[i], dp[i-1]);
+            // dp[i] = max(dp[i - 2] + nums[i], dp[i-1]);
+            int temp = firstOption + nums[i];
+            int temp2 = secondOption;
+            secondOption = max(temp, temp2);
+            firstOption = temp2;
         }
         
-        return dp[n-1];
+        // return dp[n-1];
+        return max(firstOption, secondOption);
     }
 };
