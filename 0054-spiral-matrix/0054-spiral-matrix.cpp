63765794fc05def3392fe8c6be76721c8019e442
Li-Wei Yang <68542467+liver121888@users.noreply.github.com>
2026-03-27 11:37:07 -0700

    Time: 1 ms (2.23%), Space: 9.3 MB (51.47%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -1,56 +1,41 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
+
         int m = matrix.size();
         int n = matrix[0].size();
+
+        int direction = 1;
+        int i = 0, j = -1;
+
         vector<int> ans;
-        vector<vector<bool>> visited(m, vector<bool>(n, false));
-        
-        vector<vector<int>>dir = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-        
-        int i = 0;
-        vector<int> pose = {0 , 0};
-        ans.push_back(matrix[0][0]);
-        visited[pose[0]][pose[1]] = true;
 
-        //         while(1) {
-        //             pose[0] += dir[i][0];
-        //             pose[1] += dir[i][1];
+        // two patterns
+        // move to the right
+        // then move down
+        // then move to left
+        // then move up
+        // we keep shrinking the traverse range
 
-        //             if (!visited[pose[0]][pose[1]]) {
-        //                 ans.push_back(matrix[pose[0]][pose[1]]);
-        //                 visited[pose[0]][pose[1]] = true;
-        //             } else
-        //                 break;        
+        while (m*n > 0) {
+            for (int k = 0; k < n; k++) {
+                j += direction;
 
-
-        //             if (pose[0] + dir[i][0] < 0 || pose[0] + dir[i][0] >= m || 
-        //                 pose[1] + dir[i][1] < 0 || pose[1] + dir[i][1] >= n || 
-        //                 visited[pose[0] + dir[i][0]][pose[1] + dir[i][1]]) {
-        //                 i++;
-        //             }
-
-        //             if (i == 4)
-        //                 i = 0;   
-        //         }
-        
-        while (true) {
-            int nextRow = pose[0] + dir[i][0];
-            int nextCol = pose[1] + dir[i][1];
-
-            if (nextRow >= 0 && nextRow < m && nextCol >= 0 && nextCol < n && !visited[nextRow][nextCol]) {
-                pose[0] = nextRow;
-                pose[1] = nextCol;
-                ans.push_back(matrix[pose[0]][pose[1]]);
-                visited[pose[0]][pose[1]] = true;
-            } else {
-                i = (i + 1) % 4;
+                cout << i << " " << j << endl;
+                ans.push_back(matrix[i][j]);
             }
-
-            if (ans.size() == m * n) break; // Break when all elements are visited
+            // we traverse a row, so when we go down we cannot travere m
+            m -= 1;
+            for (int k = 0; k < m; k++) {
+                i += direction;
+                cout << i << " " << j << endl;
+                ans.push_back(matrix[i][j]);
+            }
+            n -= 1;
+            direction *=-1;
         }
-
         return ans;
-        
-    }        
+
+
+    }
 };
