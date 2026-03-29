8a2c2fea2f0c341c999e6a5d4516a1e433858912
Li-Wei Yang <68542467+liver121888@users.noreply.github.com>
2026-03-29 15:22:43 -0700

    Time: 0 ms (100%), Space: 9.6 MB (31.46%) - LeetHub

---
+++ 1195-fizz-buzz-multithreaded/1195-fizz-buzz-multithreaded.cpp
@@ -0,0 +1,104 @@
+
+// 1 2 X 4 Y
+// X 7 8 X Y
+// 11 X 13 14 XY
+
+// 16 17 X 19 Y
+// ....
+
+// every time we inc we use cv varialbe to wake all thread
+// and each thread will see who can print
+
+#include <iostream>
+#include <condition_variable>
+#include <mutex>
+
+class FizzBuzz {
+private:
+    int n;
+    int curr = 1;
+    condition_variable cv;
+    mutex mtx;
+
+public:
+
+    FizzBuzz(int n) {
+        this->n = n;
+    }
+
+    // printFizz() outputs "fizz".
+    void fizz(function<void()> printFizz) {
+        while (true) {
+            unique_lock<mutex> lock(mtx);
+            cv.wait(lock, [this](){
+                if (curr > n)
+                    return true;
+                return curr % 3 == 0 && curr % 5 != 0;
+            });
+            if (curr > n)
+                break;
+            printFizz();
+            curr++;
+            cv.notify_all();
+        }
+        cv.notify_all();
+    }
+
+    // printBuzz() outputs "buzz".
+    void buzz(function<void()> printBuzz) {
+        while (true) {
+            unique_lock<mutex> lock(mtx);
+            cv.wait(lock, [this](){
+                if (curr > n)
+                    return true;
+                return curr % 3 != 0 && curr % 5 == 0;
+            });
+            if (curr > n)
+                break;
+            printBuzz();
+            curr++;
+            cv.notify_all();
+        }
+        cv.notify_all();
+    }
+
+    // printFizzBuzz() outputs "fizzbuzz".
+	void fizzbuzz(function<void()> printFizzBuzz) {
+        while (true) {
+            unique_lock<mutex> lock(mtx);
+            cv.wait(lock, [this](){
+                if (curr > n)
+                    return true;
+                return curr != 0 && curr % 3 == 0 && curr % 5 == 0;
+            });
+            if (curr > n)
+                break;
+            printFizzBuzz();
+            curr++;
+            cv.notify_all();
+        }
+        cv.notify_all();
+    }
+
+    // printNumber(x) outputs "x", where x is an integer.
+    void number(function<void(int)> printNumber) {
+
+        while (true) {
+            unique_lock<mutex> lock(mtx);
+            cv.wait(lock, [this](){
+                // game over should proceed and ends
+                if (curr > n)
+                    return true;
+                return curr % 3 != 0 && curr % 5 != 0;
+            });
+
+            if (curr > n)
+                break;
+            printNumber(curr);
+            curr++;
+            cv.notify_all();
+        }
+        cv.notify_all();
+    }
+
+};
