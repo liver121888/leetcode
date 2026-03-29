e7441cee6d1489e0e33972d97d714444c3c0cdea
Li-Wei Yang <68542467+liver121888@users.noreply.github.com>
2026-03-29 15:47:46 -0700

    Time: 7 ms (15.51%), Space: 9.4 MB (72.48%) - LeetHub

--- 1195-fizz-buzz-multithreaded/1195-fizz-buzz-multithreaded.cpp
+++ 1195-fizz-buzz-multithreaded/1195-fizz-buzz-multithreaded.cpp
@@ -13,92 +13,189 @@
 #include <condition_variable>
 #include <mutex>
 
+// class FizzBuzz {
+// private:
+//     int n;
+//     int curr = 1;
+//     condition_variable cv;
+//     mutex mtx;
+
+// public:
+
+//     FizzBuzz(int n) {
+//         this->n = n;
+//     }
+
+//     // printFizz() outputs "fizz".
+//     void fizz(function<void()> printFizz) {
+//         while (true) {
+//             unique_lock<mutex> lock(mtx);
+//             cv.wait(lock, [this](){
+//                 if (curr > n)
+//                     return true;
+//                 return curr % 3 == 0 && curr % 5 != 0;
+//             });
+//             if (curr > n)
+//                 break;
+//             printFizz();
+//             curr++;
+//             cv.notify_all();
+//         }
+//         cv.notify_all();
+//     }
+
+//     // printBuzz() outputs "buzz".
+//     void buzz(function<void()> printBuzz) {
+//         while (true) {
+//             unique_lock<mutex> lock(mtx);
+//             cv.wait(lock, [this](){
+//                 if (curr > n)
+//                     return true;
+//                 return curr % 3 != 0 && curr % 5 == 0;
+//             });
+//             if (curr > n)
+//                 break;
+//             printBuzz();
+//             curr++;
+//             cv.notify_all();
+//         }
+//         cv.notify_all();
+//     }
+
+//     // printFizzBuzz() outputs "fizzbuzz".
+// 	void fizzbuzz(function<void()> printFizzBuzz) {
+//         while (true) {
+//             unique_lock<mutex> lock(mtx);
+//             cv.wait(lock, [this](){
+//                 if (curr > n)
+//                     return true;
+//                 return curr != 0 && curr % 3 == 0 && curr % 5 == 0;
+//             });
+//             if (curr > n)
+//                 break;
+//             printFizzBuzz();
+//             curr++;
+//             cv.notify_all();
+//         }
+//         cv.notify_all();
+//     }
+
+//     // printNumber(x) outputs "x", where x is an integer.
+//     void number(function<void(int)> printNumber) {
+
+//         while (true) {
+//             unique_lock<mutex> lock(mtx);
+//             cv.wait(lock, [this](){
+//                 // game over should proceed and ends
+//                 if (curr > n)
+//                     return true;
+//                 return curr % 3 != 0 && curr % 5 != 0;
+//             });
+
+//             if (curr > n)
+//                 break;
+//             printNumber(curr);
+//             curr++;
+//             cv.notify_all();
+//         }
+//         cv.notify_all();
+//     }
+
+// };
+
+// clear version
+#include <functional>
+#include <condition_variable>
+#include <mutex>
+using namespace std;
+
 class FizzBuzz {
 private:
     int n;
-    int curr = 1;
-    condition_variable cv;
+    int curr;
     mutex mtx;
+    condition_variable cv;
 
 public:
-
-    FizzBuzz(int n) {
-        this->n = n;
-    }
+    FizzBuzz(int n) : n(n), curr(1) {}
 
     // printFizz() outputs "fizz".
     void fizz(function<void()> printFizz) {
         while (true) {
             unique_lock<mutex> lock(mtx);
-            cv.wait(lock, [this](){
-                if (curr > n)
-                    return true;
-                return curr % 3 == 0 && curr % 5 != 0;
+
+            cv.wait(lock, [this]() {
+                return curr > n || (curr % 3 == 0 && curr % 5 != 0);
             });
-            if (curr > n)
-                break;
+
+            if (curr > n) {
+                cv.notify_all();
+                return;
+            }
+
             printFizz();
-            curr++;
+            ++curr;
             cv.notify_all();
         }
-        cv.notify_all();
     }
 
     // printBuzz() outputs "buzz".
     void buzz(function<void()> printBuzz) {
         while (true) {
             unique_lock<mutex> lock(mtx);
-            cv.wait(lock, [this](){
-                if (curr > n)
-                    return true;
-                return curr % 3 != 0 && curr % 5 == 0;
+
+            cv.wait(lock, [this]() {
+                return curr > n || (curr % 3 != 0 && curr % 5 == 0);
             });
-            if (curr > n)
-                break;
+
+            if (curr > n) {
+                cv.notify_all();
+                return;
+            }
+
             printBuzz();
-            curr++;
+            ++curr;
             cv.notify_all();
         }
-        cv.notify_all();
     }
 
     // printFizzBuzz() outputs "fizzbuzz".
-	void fizzbuzz(function<void()> printFizzBuzz) {
+    void fizzbuzz(function<void()> printFizzBuzz) {
         while (true) {
             unique_lock<mutex> lock(mtx);
-            cv.wait(lock, [this](){
-                if (curr > n)
-                    return true;
-                return curr != 0 && curr % 3 == 0 && curr % 5 == 0;
+
+            cv.wait(lock, [this]() {
+                return curr > n || (curr % 3 == 0 && curr % 5 == 0);
             });
-            if (curr > n)
-                break;
+
+            if (curr > n) {
+                cv.notify_all();
+                return;
+            }
+
             printFizzBuzz();
-            curr++;
+            ++curr;
             cv.notify_all();
         }
-        cv.notify_all();
     }
 
     // printNumber(x) outputs "x", where x is an integer.
     void number(function<void(int)> printNumber) {
-
         while (true) {
             unique_lock<mutex> lock(mtx);
-            cv.wait(lock, [this](){
-                // game over should proceed and ends
-                if (curr > n)
-                    return true;
-                return curr % 3 != 0 && curr % 5 != 0;
+
+            cv.wait(lock, [this]() {
+                return curr > n || (curr % 3 != 0 && curr % 5 != 0);
             });
 
-            if (curr > n)
-                break;
+            if (curr > n) {
+                cv.notify_all();
+                return;
+            }
+
             printNumber(curr);
-            curr++;
+            ++curr;
             cv.notify_all();
         }
-        cv.notify_all();
     }
-
 };
