5ad76c48a16ebada771c9f40864c24aebd91570e
Li-Wei Yang <68542467+liver121888@users.noreply.github.com>
2026-03-29 23:40:21 -0700

    Time: 13 ms (35.21%), Space: 31.1 MB (39.64%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.cpp
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -37,9 +37,10 @@ public:
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (matrix[i][j] == '1') {
-                    dp[i][j] = 1;
-                    if (i-1 >= 0 && j-1 >= 0) {
-                        dp[i][j] = min(min(dp[i-1][j], dp[i][j-1]), dp[i-1][j-1]) + 1;
+                    if (i == 0 || j == 0) {
+                        dp[i][j] = 1;
+                    } else {
+                        dp[i][j] = min({dp[i-1][j], dp[i][j-1], dp[i-1][j-1]}) + 1;
                     }
                     edge = max(edge, dp[i][j]);
                 }
