37af15a2c10486f2389a66bf61c547ac3ff27749
Li-Wei Yang <68542467+liver121888@users.noreply.github.com>
2026-03-29 23:48:39 -0700

    Time: 0 ms (100%), Space: 27.6 MB (94.9%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.cpp
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -23,6 +23,37 @@
 // dp[i][j] the edge can be form if take this as the 
 // bottom right corner
 
+// O(mn) time
+// O(mn) space
+// class Solution {
+// public:
+//     int maximalSquare(vector<vector<char>>& matrix) {
+
+//         int m = matrix.size();
+//         int n = matrix[0].size();
+
+//         int edge = 0;
+
+//         vector<vector<int>> dp(m, vector<int>(n, 0));
+
+//         for (int i = 0; i < m; i++) {
+//             for (int j = 0; j < n; j++) {
+//                 if (matrix[i][j] == '1') {
+//                     if (i == 0 || j == 0) {
+//                         dp[i][j] = 1;
+//                     } else {
+//                         dp[i][j] = min({dp[i-1][j], dp[i][j-1], dp[i-1][j-1]}) + 1;
+//                     }
+//                     edge = max(edge, dp[i][j]);
+//                 }
+//             }
+//         }
+
+//         return edge * edge;
+//     }
+// };
+
+// O(n) space
 class Solution {
 public:
     int maximalSquare(vector<vector<char>>& matrix) {
@@ -32,21 +63,34 @@ public:
 
         int edge = 0;
 
-        vector<vector<int>> dp(m, vector<int>(n, 0));
+        vector<int> dp(n, 0);
+        // the cache for the top left corner
+        int prev = 0;
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
+
+                int temp = dp[j];
+
                 if (matrix[i][j] == '1') {
                     if (i == 0 || j == 0) {
-                        dp[i][j] = 1;
+                        dp[j] = 1;
                     } else {
-                        dp[i][j] = min({dp[i-1][j], dp[i][j-1], dp[i-1][j-1]}) + 1;
+                        int top = dp[j];
+                        int left = dp[j-1];
+                        dp[j] = min(min(top, left), prev) + 1;
                     }
-                    edge = max(edge, dp[i][j]);
+
+                    edge = max(edge, dp[j]);
+                } else {
+                    dp[j] = 0;
                 }
+
+                prev = temp;
             }
         }
 
         return edge * edge;
     }
 };
+
