06f8ebbd83301378fd1140cd443780b13fdc4246
Li-Wei Yang <68542467+liver121888@users.noreply.github.com>
2026-04-02 21:34:58 -0700

    Time: 49 ms (57.46%), Space: 105.1 MB (63.81%) - LeetHub

--- 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.cpp
+++ 2779-maximum-beauty-of-an-array-after-applying-operation/2779-maximum-beauty-of-an-array-after-applying-operation.cpp
@@ -1,37 +1,41 @@
+
+
+// maximumBeauty
+// the longest subsequce that can form
+// of the same element
+// for each element we can only operate once
+// either up or down
+// and we want to maximize the frequence of a cetain element
+
+// one way we can do is
+// traverse the array, get the range 
+// max min -> mid
+// see every element and see how many elements is up or down
+// what's bounding the beauty?
+// the min element and max element
+
+
 class Solution {
 public:
     int maximumBeauty(vector<int>& nums, int k) {
-                
-        // only need subsequence
+
+        // Hint 1
+        // Sort the array.
+        // Hint 2
+        // The problem becomes the following: find maximum subarray A[i … j] such that A[j] - A[i] ≤ 2 * k.
+
         sort(nums.begin(), nums.end());
-        
-        int result = 0;
-        
-        for (int i = 0; i < nums.size(); i++) {
-            int ub = findUpperBound(nums, nums[i]+2*k);
-            result = max(ub - i + 1, result);
-        }
-        
-        return result;
-        
-    }
-    
-    
-    int findUpperBound(vector<int>& arr, int target) {
-        int low = 0;
-        int high = arr.size() - 1;
-        int result = 0;
-        
-        while (low <= high) {
-            int mid = low + (high - low)/2;
-            if (arr[mid] <= target) {
-                result = mid;
-                low = mid + 1;
-            } else {
-                high = mid - 1;
+
+        int l = 0;
+        int ans = 0;
+        for (int r = 0; r < nums.size(); r++) {
+            while (nums[r] - nums[l] > 2 * k) {
+                // shrink
+                l++;
             }
+            ans = max(ans, r - l + 1);
         }
-        
-        return result;
+
+        return ans;        
     }
 };
