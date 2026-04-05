a40cab0ce8400e500a4872620a8240d165c98346
Li-Wei Yang <68542467+liver121888@users.noreply.github.com>
2026-04-05 12:18:48 -0700

    Time: 0 ms (100%), Space: 10.3 MB (8.89%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -16,40 +16,68 @@ public:
     // j = 1
 
     // transpose
-    void transpose(vector<vector<int>> & matrix) {
+    // void transpose(vector<vector<int>> & matrix) {
 
-        int n = matrix.size();
-        // diag traverse
-        // main diagonal: i - j = same number
-        // i >= diag
-        for (int diag = 0; diag < n; diag++) {
-            for (int i = diag; i < n; i++) {
-                int j = i - diag;
-                swap(matrix[i][j], matrix[j][i]);
-            }
-        }
-    }
+    //     int n = matrix.size();
+    //     // diag traverse
+    //     // main diagonal: i - j = same number
+    //     // i >= diag
+    //     for (int diag = 0; diag < n; diag++) {
+    //         for (int i = diag; i < n; i++) {
+    //             int j = i - diag;
+    //             swap(matrix[i][j], matrix[j][i]);
+    //         }
+    //     }
+    // }
 
-    void mirrorVertical(vector<vector<int>> & matrix) {
-        int n = matrix.size();
-        int l = 0;
-        int r = n - 1;
-        while (l < r) {
-            for (int i = 0; i < n; i++) {
-                swap(matrix[i][l], matrix[i][r]);
-            }
-            l++;
-            r--;
-        }
-    }
+    // void mirrorVertical(vector<vector<int>> & matrix) {
+    //     int n = matrix.size();
+    //     int l = 0;
+    //     int r = n - 1;
+    //     while (l < r) {
+    //         for (int i = 0; i < n; i++) {
+    //             swap(matrix[i][l], matrix[i][r]);
+    //         }
+    //         l++;
+    //         r--;
+    //     }
+    // }
 
+    // void rotate(vector<vector<int>>& matrix) {
+    //     transpose(matrix);
+    //     mirrorVertical(matrix);        
+    // }
+
+
+    // four in a group
+    // 1 3 9 7
+    // 0,0 0,n-1 n-1,n-1 n-1,0
+
+    // 2 6 8 4
+    // 0,1 1,n-1 n-1,n-2 n-2,0
 
     void rotate(vector<vector<int>>& matrix) {
-        transpose(matrix);
-        // for (int i = 0; i < matrix.size(); i++)
-        //     for (int j = 0; j< matrix.size(); j++)
-        //         cout << matrix[i][j] << " ";
-        // cout << endl;
-        mirrorVertical(matrix);        
+
+        // n = 4
+        // i = 0
+        // 3,0/3,3/0,3,0,0
+
+        // i = 1
+        // 2,1/2,2
+
+        int n = matrix.size();
+        for (int i = 0; i < (n + 1) / 2; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int temp = matrix[n - 1 - j][i];
+                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - j - 1];
+                matrix[n - 1 - i][n - j - 1] = matrix[j][n - 1 - i];
+                matrix[j][n - 1 - i] = matrix[i][j];
+                matrix[i][j] = temp;
+            }
+        }
+
     }
+
+
+
 };
