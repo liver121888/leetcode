b021b2daec5d1c2881dbb37b4487acb1a77a5809
Li-Wei Yang <68542467+liver121888@users.noreply.github.com>
2026-04-06 17:11:34 -0700

    Time: 23 ms (89.82%), Space: 14.7 MB (84%) - LeetHub

--- 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.cpp
@@ -1,9 +1,9 @@
 
+// prim
 // 從某個點開始
 // 每次拿最小 cost 的點
 // 如果這點已經在 MST，跳過
 // 如果不在，把它加入 MST，然後把鄰邊丟進 pq
-
 
 class Solution {
 public:
@@ -12,10 +12,14 @@ public:
         vector<int> minDist(n, INT_MAX);
         vector<bool> inMST(n, false);
 
+        // 把0放進來
         minDist[0] = 0;
         int ans = 0;
 
+        // 做 n 次
         for (int i = 0; i < n; i++) {
+
+            // Step 1：找最便宜能接進來的點 u
             int u = -1;
             for (int j = 0; j < n; j++) {
                 if (!inMST[j] && (u == -1 || minDist[j] < minDist[u])) {
@@ -23,9 +27,11 @@ public:
                 }
             }
 
+            // Step 2：把它加入 MST
             inMST[u] = true;
             ans += minDist[u];
 
+            // Step 3：用 u 更新其他點
             for (int v = 0; v < n; v++) {
                 if (!inMST[v]) {
                     int d = abs(points[u][0] - points[v][0]) +
