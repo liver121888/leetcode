ed46de6f9147a0470438a7e006ff5677e3e21452
Li-Wei Yang <68542467+liver121888@users.noreply.github.com>
2026-04-12 16:24:30 -0700

    Time: 0 ms (100%), Space: 27.4 MB (42.09%) - LeetHub

---
+++ 3592-inverse-coin-change/3592-inverse-coin-change.cpp
@@ -0,0 +1,37 @@
+
+class Solution {
+    
+public:
+    std::vector<int> findCoins(const std::vector<int>& numWays) {
+        int n = static_cast<int>(numWays.size());
+        std::vector<long long> myWays(n + 1, 0);
+        myWays[0] = 1;
+
+        std::vector<int> res;
+
+        for (int i = 1; i <= n; ++i) {
+            // If myWays[x] == numWays[x], move on.
+            if (myWays[i] == numWays[i - 1]) continue;
+
+            // If myWays[x] + 1 == numWays[x]
+            // add that value as a coin in our basket and 
+            // update `myWays`, so `myWays[x...n]` accounts for ways with the new coin.
+            if (numWays[i - 1] - myWays[i] == 1) {
+                res.push_back(i);
+                // update myWays
+                for (int j = i; j <= n; ++j) {
+                    myWays[j] += myWays[j - i];
+                }
+
+            // If `myWays[x] + 1 < numWays[x]
+            // no solution.
+            } else {
+                return {};
+            }
+        }
+        return res;
+    }
+
+
+};
+
