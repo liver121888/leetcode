95423abae68d74bbbce46487b8e3091471e09d73
Li-Wei Yang <68542467+liver121888@users.noreply.github.com>
2026-04-12 15:32:25 -0700

    Time: 5 ms (88.64%), Space: 10.4 MB (77.91%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,28 +1,65 @@
+
+// coin change i
+// minimum numbers
+
+// Return the number of combinations that make up that amount. 
+// dp[i] = the ways to make up that amount
+// dp[i] = sum (for j in range coins dp[i - coins[j]])
+
+// base case: 1 way to make amount 0
+
+// The answer is guaranteed to fit into a signed 32-bit integer.
+// so we don't need a long long
+
+//  amount = 5, coins = [1,2,5]
+// dp 1 0 0 0 0 0
+// dp 1 1 0 0 0 0
+// dp 1 1 2 0 0 0
+// dp 1 1 2 2 0 0
+// dp 1 1 2 2 3 4
+// 
+
+using ull = unsigned long long;
+
 class Solution {
 public:
     int change(int amount, vector<int>& coins) {
+
         int n = coins.size();
-        vector<vector<int>> dp(n, vector<int>(amount + 1, -1));
-        return dfs(dp, coins, 0, 0);
+        vector<ull> dp(amount+1, 0);
+        // base case, one way to get amount 0
+        dp[0] = 1;
+
+        // 外層先枚舉 coin，內層再枚舉 amount。
+        // 這樣就會保證：
+        // 1 + 2 + 2
+        // 2 + 1 + 2
+        // 2 + 2 + 1
+        // 這些不同順序，只會被算成 同一種組合。
+
+        // for (int i = 1; i <= amount; i++) {
+        //     // try every coin, the way we try is important
+        //     // we don't want eg 3
+        //     // 2 + 1
+        //     // 1 + 2 to be count twice
+        //     // we can use unorder map to save the numbers we used
+        //     // 
+        //     for (int j = 0; j < n; j++) {
+        //         if (i - coins[j] >= 0)
+        //             dp[i] += dp[i - coins[j]];
+        //     }
+        // }
+
+        // 外圈 iter coins就確保了不會重複
+        // 意義是現在能用這些coins你能用幾種方式湊出amount
+        for (int j = 0; j < n; j++) {
+            // amount至少從coins[j]開始
+            for (int i = coins[j]; i <= amount; i++) {
+                dp[i] += dp[i - coins[j]];
+            }
+        }
+
+        return dp[amount];
     }
-    
-    int dfs(vector<vector<int>>& dp, vector<int>& coins, int i, int j) {       
-        // how many ways to sum up to zero? Always 1
-        if (j == dp[0].size() - 1)
-            return 1;
-        
-        if (j > dp[0].size() - 1)
-            return 0;
-        
-        if (i == coins.size())
-            // out of bound
-            return 0;
-        
-        if (dp[i][j] != -1)
-            return dp[i][j];
-        
-        
-        dp[i][j] = dfs(dp, coins, i, j + coins[i]) + dfs(dp, coins, i + 1, j);        
-        return dp[i][j];        
-    }    
+
 };
