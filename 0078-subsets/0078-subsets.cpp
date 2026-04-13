45380e0497d246d95514b5e01b15a5173b361d7f
Li-Wei Yang <68542467+liver121888@users.noreply.github.com>
2026-04-13 15:04:48 -0700

    Time: 0 ms (100%), Space: 9.9 MB (80.6%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,26 +1,38 @@
+
+// all possible subsets (the power set).
+// All the numbers of nums are unique.
+// we can back track and put in the numbers
+// sequence is not important here
+// all numbers are unique
+// at every position we can either choose to put in 
+// or not put in
+// time complexity O(2^n)
+// space: O(n) because of the recursion stack 
 class Solution {
 public:
-    vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> ans;
-        vector<int> curState;
-        dfs(ans, nums, curState, 0);
-        return ans;
-    }
-    
-    void dfs(vector<vector<int>>& ans, vector<int>& nums, 
-             vector<int>& curState, int i) {
-        if (i >= nums.size()) {
-            ans.push_back(curState);
+
+    void backtrack(vector<vector<int>>& ans, vector<int>& state, 
+        const vector<int>& nums, int i) {
+
+        // base case
+        if (i == nums.size()) {
+            ans.push_back(state);
             return;
         }
-        
-        // two subproblem
-        // with cur num
-        curState.push_back(nums[i]);
-        dfs(ans, nums, curState, i + 1);
-        // without cur num
-        curState.pop_back();
-        dfs(ans, nums, curState, i + 1);
+
+        state.push_back(nums[i]);
+        backtrack(ans, state, nums, i+1);
+        state.pop_back();
+        backtrack(ans, state, nums, i+1);
+
     }
-    
+
+    vector<vector<int>> subsets(vector<int>& nums) {
+
+        vector<vector<int>> ans;
+        vector<int> state;
+        backtrack(ans, state, nums, 0);
+        return ans;
+        
+    }
 };
