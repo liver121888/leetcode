f61057c60587bcae75f3aed01cec3ed14cc2ee53
Li-Wei Yang <68542467+liver121888@users.noreply.github.com>
2026-04-13 19:41:06 -0700

    Time: 3 ms (69.85%), Space: 16 MB (25.52%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,28 +1,47 @@
+
+// Given n pairs of parentheses, write a function to 
+// generate all combinations of well-formed parentheses
+
+// we have to put an open bracket at first
+// and then we can choose when to close it
+// at any given state, the ( and ) should have equal counts
+// or you can say ) cnt cannot be larger than ( cnt
+// and the ( canot be larger than n
+// the recursion ends when the size is n*2
+
+// time: O(2^n)
+// space: O(n) for temp
+
 class Solution {
 public:
-    vector<string> generateParenthesis(int n) {
-        vector<string> ans;
 
-        vector<char> curString;
-        backTracking(ans, curString, 0, 0, n);
-        return ans;
+    vector<string> ans;
+
+    void backtrack(string s, int n, int openCnt, int closeCnt) {
+
+        // base case: judge by size, when i+1 > 2*n
+        if (openCnt + closeCnt == 2 * n) {
+            ans.push_back(s);
+            return;
+        }
+
+
+        if (openCnt == n) {
+            backtrack(s+')', n, openCnt, closeCnt+1);
+            return;
+        }
+
+        if (closeCnt <= openCnt) {
+            backtrack(s+')', n, openCnt, closeCnt+1);
+            backtrack(s+'(', n, openCnt+1, closeCnt);
+        }
+
     }
-    
-    void backTracking(vector<string>& ans, vector<char>& curString, int openCnt, int closeCnt, int& n) {
-        if (openCnt == closeCnt && openCnt == n) {
-            ans.push_back(string(curString.begin(), curString.end()));
-        }
-        
-        if (openCnt < n) {
-            curString.push_back('(');
-            backTracking(ans, curString, openCnt + 1, closeCnt, n);
-            curString.pop_back();          
-        }
 
-        if (closeCnt < openCnt) {
-            curString.push_back(')');
-            backTracking(ans, curString, openCnt, closeCnt + 1, n);
-            curString.pop_back();
-        }        
+    vector<string> generateParenthesis(int n) {
+        ans.clear(); 
+        string state = "";
+        backtrack(state, n, 0, 0);
+        return ans;
     }
 };
