3842c07c611d6257b59c58716d36db056e106c12
Li-Wei Yang <68542467+liver121888@users.noreply.github.com>
2026-04-14 13:56:24 -0700

    Time: 707 ms (30.78%), Space: 242.3 MB (8.56%) - LeetHub

--- 0363-max-sum-of-rectangle-no-larger-than-k/0363-max-sum-of-rectangle-no-larger-than-k.cpp
+++ 0363-max-sum-of-rectangle-no-larger-than-k/0363-max-sum-of-rectangle-no-larger-than-k.cpp
@@ -4,23 +4,34 @@
 class Solution {
 public:
     int maxSumSubmatrix(vector<vector<int>>& matrix, int k) {
+
         int m = matrix.size(), n = matrix[0].size();
         int result = INT_MIN;
 
+        // iter every possible
+        // start with r1 and ends with r2
+        // O(m^2)
+
         for (int r1 = 0; r1 < m; r1++) {
+
             vector<int> col_sum(n, 0);
             
             for (int r2 = r1; r2 < m; r2++) {
+                // O(n) to sum up
                 for (int col = 0; col < n; col++) {
                     col_sum[col] += matrix[r2][col];
                 }
 
+                // ordered set to sort the sums
+                // O(nlogn) to look through possible sums
                 set<int> prefix_sums;
+                // always a 0 sum
                 prefix_sums.insert(0);
                 int curr_sum = 0;
                 
                 for (int sum : col_sum) {
                     curr_sum += sum;
+                    // 1d subarray equal k
                     auto it = prefix_sums.lower_bound(curr_sum - k);
                     if (it != prefix_sums.end()) {
                         result = max(result, curr_sum - *it);
