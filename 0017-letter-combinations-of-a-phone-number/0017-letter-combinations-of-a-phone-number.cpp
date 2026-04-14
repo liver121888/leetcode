4f1295c7a7bf41974510b745f283c4403c9d86ed
Li-Wei Yang <68542467+liver121888@users.noreply.github.com>
2026-04-13 19:07:50 -0700

    Time: 1 ms (18.79%), Space: 9.7 MB (9.3%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,57 @@
+
+//  return all possible letter combinations that the number could represent. 
+// Return the answer in any order.
+
+// we can do a backtracking algorithm
+// a unorderred_map to record the relation
+// recursion
+// 3 - 4
+// time: O(4^n * n)
+// space: O(n)
+
+class Solution {
+public:
+
+    const unordered_map<char, vector<char>> keyboard = {
+        {'2', {'a','b','c'}},
+        {'3', {'d','e','f'}},
+        {'4', {'g','h','i'}},
+        {'5', {'j','k','l'}},
+        {'6', {'m','n','o'}},
+        {'7', {'p','q','r', 's'}},
+        {'8', {'t','u','v'}},
+        {'9', {'w','x','y','z'}}
+    };
+
+    vector<string> ans;
+
+    void backtrack(vector<char>& state, string digits, int i) {
+
+        // base case
+        if (i == digits.length()) {
+            string temp = "";
+            for (const auto& c : state) {
+                temp += c;  
+            }
+            ans.push_back(temp);
+            return;
+        }
+
+        char currDigit = digits[i];
+        // cout << currDigit << endl;
+        for (const auto& c : keyboard.at(currDigit)) {
+            state.push_back(c);
+            backtrack(state, digits, i+1);
+            // restore state
+            state.pop_back();
+        }
+
+    }
+
+    vector<string> letterCombinations(string digits) {
+        ans.clear();
+        vector<char> state;
+        backtrack(state, digits, 0);
+        return ans;
+    }
+};
