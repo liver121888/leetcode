aadef7cb91b760911a87fa6cd2db9d25c7b3a1b3
Li-Wei Yang <68542467+liver121888@users.noreply.github.com>
2026-04-15 10:16:54 -0700

    Time: 0 ms (100%), Space: 18.2 MB (68.33%) - LeetHub

---
+++ 2515-shortest-distance-to-target-string-in-a-circular-array/2515-shortest-distance-to-target-string-in-a-circular-array.cpp
@@ -0,0 +1,32 @@
+
+// there might be repeated words
+// we can traverse the array and 
+// count the minimum target in both ways
+
+// ["hello","i","am","leetcode","hello"]
+// startIndex = 1
+// [0 1 2 3 4] [5 6 7 8 9]
+// 1 4 3 2
+
+class Solution {
+public:
+    int closestTarget(vector<string>& words, string target, int startIndex) {
+        
+        if (words[startIndex] == target)
+            return 0;
+        int n = words.size();
+        int minDist = INT_MAX;
+        for (int i = 0; i < n; i++) {
+            if (words[i] == target) {
+                if (i > startIndex) {
+                    minDist = min(i - startIndex, minDist);
+                    minDist = min(startIndex + n - i, minDist);
+                } else {
+                    minDist = min(startIndex - i, minDist);
+                    minDist = min(i + n - startIndex,minDist);
+                }
+            }
+        }
+        return minDist == INT_MAX ? -1 : minDist;
+    }
+};
