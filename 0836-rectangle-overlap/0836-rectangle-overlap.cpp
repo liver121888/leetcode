010e94e5d3308c44cc741c255aa5ebdcd56e06a4
Li-Wei Yang <68542467+liver121888@users.noreply.github.com>
2026-04-14 17:35:49 -0700

    Time: 0 ms (100%), Space: 9.8 MB (46.94%) - LeetHub

---
+++ 0836-rectangle-overlap/0836-rectangle-overlap.cpp
@@ -0,0 +1,22 @@
+
+
+// To be clear, two rectangles that only touch at the corner or edges do not overlap.
+
+class Solution {
+public:
+    bool isRectangleOverlap(vector<int>& rec1, vector<int>& rec2) {
+
+        int x1A = rec1[0]; int y1A = rec1[1]; int x2A = rec1[2]; int y2A = rec1[3];        
+        int x1B = rec2[0]; int y1B = rec2[1]; int x2B = rec2[2]; int y2B = rec2[3];
+
+        bool xOverlap = false;
+        if (max(x1A, x1B) < min(x2A, x2B))
+            xOverlap = true;
+        bool yOverlap = false;
+        if (max(y1A, y1B) < min(y2A, y2B))
+            yOverlap = true;
+        if (xOverlap && yOverlap)
+            return true;
+        return false;
+    }
+};
