37d9bf8ae6033c3e8b93dc504126c5e17b5b5ff8
Li-Wei Yang <68542467+liver121888@users.noreply.github.com>
2026-04-18 15:24:59 -0700

    Time: 60 ms (8.17%), Space: 108.7 MB (9.67%) - LeetHub

---
+++ 2381-shifting-letters-ii/2381-shifting-letters-ii.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+
+    string shiftingLetters(string s, vector<vector<int>>& shifts)
+    {
+        int n = s.size();
+        vector<int> diff(n);
+
+        for (auto shift : shifts) {
+            if (shift[2]) {
+                diff[shift[0]]++;
+
+                if (shift[1] + 1 < n) {
+                    diff[shift[1] + 1]--;
+                }
+
+            } else {
+                diff[shift[0]]--;
+
+                if (shift[1] + 1 < n) {
+                    diff[shift[1] + 1]++;
+                }
+            }
+        }
+
+        string result(n, ' ');
+        int numOfShifts = 0;
+
+        for (int i = 0; i < s.size(); i++) {
+            numOfShifts = (numOfShifts + diff[i]) % 26;
+
+            if (numOfShifts < 0)
+                numOfShifts += 26;
+
+            result[i] = 'a' + (s[i] - 'a' + numOfShifts) % 26;
+
+        }
+
+        return result;
+
+    }
+};
