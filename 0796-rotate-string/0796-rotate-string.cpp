387e08e0d7333c1dd33cdf57c9ffea7f90c0a617
Li-Wei Yang <68542467+liver121888@users.noreply.github.com>
2026-05-04 15:01:52 -0700

    Time: 0 ms (100%), Space: 8.2 MB (62.3%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -16,55 +16,64 @@
 // O(n * n), n here is to traverse to check
 // O(1) space, we only traverse
 
-// prune: we only start checking if the start character is the same
-
+// concate, some extra space but easy to understand
 class Solution {
 public:
-
-    bool checkString(const string& s, const string& g, int i) {
-
-        int n = s.size();
-        // int windowLen = 0;
-        // int j = 0;
-        // while (windowLen < n) {
-
-        //     if (i >= n)
-        //         i = 0;
-
-        //     if (s[i] != g[j])
-        //         return false;
-        //     j++;
-        //     i++;
-        //     windowLen++;
-        // }
-        // return true;
-
-        for (int k = 0; k < n; k++) {
-            if (s[(i + k) % n] != g[k])
-                return false;
-        }
-        return true;
-    }
-
-
     bool rotateString(string s, string goal) {
-
-        if (s.length() != goal.length())
-            return false;
-
-        // check empty string
-        if (s.empty() && goal.empty())
-            return true;
-
-        int n = s.length();
-        // bool valid = false;
-        for (int i = 0; i < n; i++) {
-            // if (s[i] == goal[0])
-            //     valid |= checkString(s, goal, i);
-            if (s[i] == goal[0])
-                if (checkString(s, goal, i))
-                    return true;
-        }
-        return false;
+        return s.size() == goal.size() &&
+               (s + s).find(goal) != string::npos;
     }
 };
+
+
+// prune: we only start checking if the start character is the same
+// class Solution {
+// public:
+
+//     bool checkString(const string& s, const string& g, int i) {
+
+//         int n = s.size();
+//         // int windowLen = 0;
+//         // int j = 0;
+//         // while (windowLen < n) {
+
+//         //     if (i >= n)
+//         //         i = 0;
+
+//         //     if (s[i] != g[j])
+//         //         return false;
+//         //     j++;
+//         //     i++;
+//         //     windowLen++;
+//         // }
+//         // return true;
+
+//         for (int k = 0; k < n; k++) {
+//             if (s[(i + k) % n] != g[k])
+//                 return false;
+//         }
+//         return true;
+//     }
+
+
+//     bool rotateString(string s, string goal) {
+
+//         if (s.length() != goal.length())
+//             return false;
+
+//         // check empty string
+//         if (s.empty() && goal.empty())
+//             return true;
+
+//         int n = s.length();
+//         // bool valid = false;
+//         for (int i = 0; i < n; i++) {
+//             // if (s[i] == goal[0])
+//             //     valid |= checkString(s, goal, i);
+//             if (s[i] == goal[0])
+//                 if (checkString(s, goal, i))
+//                     return true;
+//         }
+//         return false;
+//     }
+// };
