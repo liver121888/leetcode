13bf6e76b1bdca523c104d5e8bb3f21176177ad0
Li-Wei Yang <68542467+liver121888@users.noreply.github.com>
2026-05-04 14:39:54 -0700

    Time: 0 ms (100%), Space: 10 MB (92.67%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,83 +1,47 @@
-// an n x n 2D matrix
-// always square
-// we have two way to rotate
-// one way to do this is to do rotation on group of 4
-// another way is to transpose then mirror vertically
+// to rotate image
+// we can rotate in 4 of a groups
+// 0,0, 0,2, 2,2, 2,0
+// 0,1, 1,2, 2,1, 1,0
+// time: O(m*n), because we traverse from 0 to n-1
+// space: O(1), in-place
+
+// we can also do a transpose first
+// then mirror horizontally
+// time: O(m*n) for transpose, O(m*n) for mirror
+// space: O(1)
+
+// is the matrix valid?
+// yes, you can always assume that it's valid (2-dim)
+// is the size of the matrix within int?
+// yes
 
 class Solution {
 public:
-
-    // n = 2
-    // diag = 1
-    // i = 0
-    // j = 0
-
-    // i = 1
-    // j = 1
-
-    // transpose
-    // void transpose(vector<vector<int>> & matrix) {
-
-    //     int n = matrix.size();
-    //     // diag traverse
-    //     // main diagonal: i - j = same number
-    //     // i >= diag
-    //     for (int diag = 0; diag < n; diag++) {
-    //         for (int i = diag; i < n; i++) {
-    //             int j = i - diag;
-    //             swap(matrix[i][j], matrix[j][i]);
-    //         }
-    //     }
-    // }
-
-    // void mirrorVertical(vector<vector<int>> & matrix) {
-    //     int n = matrix.size();
-    //     int l = 0;
-    //     int r = n - 1;
-    //     while (l < r) {
-    //         for (int i = 0; i < n; i++) {
-    //             swap(matrix[i][l], matrix[i][r]);
-    //         }
-    //         l++;
-    //         r--;
-    //     }
-    // }
-
+    // 4 of a group method
     // void rotate(vector<vector<int>>& matrix) {
-    //     transpose(matrix);
-    //     mirrorVertical(matrix);        
+        
     // }
 
-
-    // four in a group
-    // 1 3 9 7
-    // 0,0 0,n-1 n-1,n-1 n-1,0
-
-    // 2 6 8 4
-    // 0,1 1,n-1 n-1,n-2 n-2,0
-
+    // matrix operation method
     void rotate(vector<vector<int>>& matrix) {
+        
+        int m = matrix.size();
+        int n = matrix[0].size();
 
-        // n = 4
-        // i = 0
-        // 3,0/3,3/0,3,0,0
+        // transpose
+        for (int diag = 0; diag < m; diag++) {
+            for (int i = diag; i < m; i++) {
+                swap(matrix[diag][i], matrix[i][diag]);
+            }
+        }
 
-        // i = 1
-        // 2,1/2,2
-
-        int n = matrix.size();
-        for (int i = 0; i < (n + 1) / 2; i++) {
-            for (int j = 0; j < n / 2; j++) {
-                int temp = matrix[n - 1 - j][i];
-                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - j - 1];
-                matrix[n - 1 - i][n - j - 1] = matrix[j][n - 1 - i];
-                matrix[j][n - 1 - i] = matrix[i][j];
-                matrix[i][j] = temp;
+        // mirror horizontally
+        for (int j = 0; j < n/2; j++) {
+            for (int i = 0; i < m; i++) {
+                swap(matrix[i][j], matrix[i][n-1-j]);
             }
         }
 
     }
-
-
 
 };
