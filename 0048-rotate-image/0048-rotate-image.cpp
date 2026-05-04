1cea8afd4a6ab62f27df5373ff95b91e12869234
Li-Wei Yang <68542467+liver121888@users.noreply.github.com>
2026-05-04 14:41:44 -0700

    Time: 3 ms (1.57%), Space: 10.2 MB (35.38%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -2,6 +2,7 @@
 // we can rotate in 4 of a groups
 // 0,0, 0,2, 2,2, 2,0
 // 0,1, 1,2, 2,1, 1,0
+// 1,1
 // time: O(m*n), because we traverse from 0 to n-1
 // space: O(1), in-place
 
@@ -18,30 +19,38 @@
 class Solution {
 public:
     // 4 of a group method
-    // void rotate(vector<vector<int>>& matrix) {
-        
-    // }
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        for (int i = 0; i < (n + 1) / 2; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int temp = matrix[n - 1 - j][i];
+                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - j - 1];
+                matrix[n - 1 - i][n - j - 1] = matrix[j][n - 1 - i];
+                matrix[j][n - 1 - i] = matrix[i][j];
+                matrix[i][j] = temp;
+            }
+        }
+    }
 
     // matrix operation method
-    void rotate(vector<vector<int>>& matrix) {
+    // void rotate(vector<vector<int>>& matrix) {
         
-        int m = matrix.size();
-        int n = matrix[0].size();
+    //     int m = matrix.size();
+    //     int n = matrix[0].size();
 
-        // transpose
-        for (int diag = 0; diag < m; diag++) {
-            for (int i = diag; i < m; i++) {
-                swap(matrix[diag][i], matrix[i][diag]);
-            }
-        }
+    //     // transpose
+    //     for (int diag = 0; diag < m; diag++) {
+    //         for (int i = diag; i < m; i++) {
+    //             swap(matrix[diag][i], matrix[i][diag]);
+    //         }
+    //     }
 
-        // mirror horizontally
-        for (int j = 0; j < n/2; j++) {
-            for (int i = 0; i < m; i++) {
-                swap(matrix[i][j], matrix[i][n-1-j]);
-            }
-        }
-
-    }
+    //     // mirror horizontally
+    //     for (int j = 0; j < n/2; j++) {
+    //         for (int i = 0; i < m; i++) {
+    //             swap(matrix[i][j], matrix[i][n-1-j]);
+    //         }
+    //     }
+    // }
 
 };
