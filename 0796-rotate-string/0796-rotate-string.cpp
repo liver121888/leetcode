0dd99727eb5619e9637c524a0e342f5c61225e04
Li-Wei Yang <68542467+liver121888@users.noreply.github.com>
2026-05-04 14:57:06 -0700

    Time: 0 ms (100%), Space: 8.1 MB (62.3%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -0,0 +1,60 @@
+
+// can there be repeat charachter
+// yes
+// is there finite set for the char exist in the string?
+// yes, only lower case
+// is the size of the string in int?
+// yes
+// will there be empty string?
+// yes
+
+// brute force
+// l,r substring
+// compare
+// l r should always maintain size n
+// try different start position
+// O(n * n), n here is to traverse to check
+// O(1) space, we only traverse
+
+// prune: we only start checking if the start character is the same
+
+class Solution {
+public:
+
+    bool checkString(const string& s, const string& g, int i) {
+        int n = s.size();
+        int windowLen = 0;
+        int j = 0;
+        while (windowLen < n) {
+
+            if (i >= n)
+                i = 0;
+
+            if (s[i] != g[j])
+                return false;
+            j++;
+            i++;
+            windowLen++;
+        }
+        return true;
+    }
+
+
+    bool rotateString(string s, string goal) {
+
+        if (s.length() != goal.length())
+            return false;
+
+        // check empty string
+        if (s.empty() && goal.empty())
+            return true;
+
+        int n = s.length();
+        bool valid = false;
+        for (int i = 0; i < n; i++) {
+            if (s[i] == goal[0])
+                valid |= checkString(s, goal, i);
+        }
+        return valid;
+    }
+};
