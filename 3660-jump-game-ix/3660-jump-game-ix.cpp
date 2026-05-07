95ef5775b639d7e1c5523b6d74cba74649464451
Li-Wei Yang <68542467+liver121888@users.noreply.github.com>
2026-05-06 23:11:27 -0700

    Time: 21 ms (30.32%), Space: 225.7 MB (53.19%) - LeetHub

--- 3660-jump-game-ix/3660-jump-game-ix.cpp
+++ 3660-jump-game-ix/3660-jump-game-ix.cpp
@@ -48,6 +48,21 @@ public:
         int start = 0;
         int segmentMax = nums[0];
 
+        // 所以只要看：
+        // max(left side) > min(right side)
+        // 也就是：
+        // prefixMax[i] > suffixMin[i + 1]
+        // 如果成立，代表左右兩邊有機會連起來。
+        // 如果不成立：
+        // prefixMax[i] <= suffixMin[i + 1]
+        // 代表左邊所有值都 <= 右邊所有值，兩邊完全不能互跳，可以切開。
+        // 所以答案可以把 array 切成幾段，每一段裡面的 index 答案都是這段的最大值。
+        // 例如：
+        // nums = [1, 3, 2, 4]
+        // [1] 跟 [3,2] 跟 [4] 可以切開。
+        // 答案：
+        // [1, 3, 3, 4]
+
         for (int i = 0; i < n; i++) {
             segmentMax = max(segmentMax, nums[i]);
 
