7b6d3106d8bf4d96ea8518c5e285bf0349da8000
Li-Wei Yang <68542467+liver121888@users.noreply.github.com>
2026-05-09 14:36:52 -0700

    Time: 5 ms (83.24%), Space: 11.3 MB (85.14%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -6,65 +6,60 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+
+// we can have a fast and slow pointer
+// if no cycle, fast pointer will reach null
+// if has cycle we break when fast == slow
+// start from the head again
+// traverse at same speed
+// the ans will be the ponint they meet again
+
+// 3 2 0 -4
+// 3 3
+// 2 0
+// 0 2
+// -4 -4
+// cycle
+// 3 -4
+// 2 2
+// 2 is the entry point
+// why?
+
+// traverse a
+// still has b
+// 2(c+a) = c + a + b + a
+// 2c + 2a = c + 2a + b
+// c = c + b
+
 class Solution {
 public:
-
-
-    // 3 2 0 -4
-    // slow -4
-    // fast -4
-
-    // slow 3
-    // fast -4
-    // fast 2
-    // slow 2
-    // return fast
-
-    // 1 2
-    // slow 1
-    // fast 1
-    // slow 2
-    // fast 1
-
-    // slow 1
-    // fast 1
-    
-    // slow
-    // fast
-
-    
-
     ListNode *detectCycle(ListNode *head) {
+
+        if (!head || !head->next)
+            return nullptr;
 
         ListNode* fast = head;
         ListNode* slow = head;
 
         bool hasCycle = false;
-        while(fast && fast->next) {
+        while (fast && fast->next) {
             fast = fast->next->next;
             slow = slow->next;
-
-            // we check after we move
-            // otherwise we won't even start the algo
             if (fast == slow) {
                 hasCycle = true;
                 break;
             }
         }
-
         if (!hasCycle)
             return nullptr;
 
-        // fast will be the intersection point
-        // we start the fast as slow and let the slow 
-        // has time to catch up
-
-        slow = head;
-        while (slow != fast) {
-            slow = slow->next;
+        fast = head;
+        while (fast != slow) {
             fast = fast->next;
+            slow = slow->next;
         }
 
-        return fast;
+        return slow;
+        
     }
 };
