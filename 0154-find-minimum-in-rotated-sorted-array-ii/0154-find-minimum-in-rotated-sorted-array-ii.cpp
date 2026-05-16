693688d316b5551dcf99b6bd2afbc658e1023a25
Li-Wei Yang <68542467+liver121888@users.noreply.github.com>
2026-05-16 11:10:48 -0700

    Time: 0 ms (100%), Space: 16.2 MB (27.34%) - LeetHub

---
+++ 0154-find-minimum-in-rotated-sorted-array-ii/0154-find-minimum-in-rotated-sorted-array-ii.cpp
@@ -0,0 +1,52 @@
+// return the minimum element of this array.
+
+// [2,2,2,0,1]
+// n = 5
+// l = 0 -> 2
+// r = 4 -> 1
+// mid = 2 -> 2
+
+// mid ~ l is sorted
+
+
+
+
+class Solution {
+public:
+    // that may contain duplicates
+    int findMin(vector<int>& nums) {
+
+        int n = nums.size();
+
+        // no rotation 特判
+        if (nums[0] < nums[n - 1]) 
+            return nums[0];
+
+        // has rotation
+        // so the array becomes 2 segments
+        // first increase seq
+        // second increase seq
+        int l = 0;
+        int r = n - 1;
+
+        // then here, we are finding the first index makes nums[i] < nums[0]
+        // [2,2,2,0,1]
+
+        while (l < r) {
+
+            int mid = (l + r)/2;
+
+            if (nums[mid] < nums[0]) {
+                r = mid;
+            } else if (nums[mid] > nums[r]) {
+                l = mid + 1;
+            } else {
+                r--;
+            }
+
+        }
+        return nums[l];
+
+    }
+};
+
