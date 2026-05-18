80cccb7c29a269888a3a450399985ee11f96582f
Li-Wei Yang <68542467+liver121888@users.noreply.github.com>
2026-05-18 00:17:22 -0700

    Time: 19 ms (78.75%), Space: 97.5 MB (10.56%) - LeetHub

--- 0164-maximum-gap/0164-maximum-gap.cpp
+++ 0164-maximum-gap/0164-maximum-gap.cpp
@@ -19,6 +19,31 @@ public:
         int maxval = numeric_limits<int>::min();  // same as INT_MIN
     };
 
+    // k is the number of buckets
+    // Time complexity: O(n+k)≈O(n), traverse nums first, then we traverse buckets that has size k
+    // Space complexity: O(2⋅k)≈O(k) extra space, each bucket saves two numbers
+
+    // 先看最佳情況：假設陣列排序後，每一對相鄰元素之間的距離都一樣。
+    // 也就是說，每個相鄰元素之間都差同一個固定值。
+    // 如果陣列有 n 個元素，那排序後會有 n - 1 個 gap。假設每個 gap 寬度都是 t0。
+    // 那麼可以很容易得到：
+    // t0 = (max - min) / (n - 1)
+    // 其中 max 和 min 分別是陣列中的最大值與最小值。
+    // 這個寬度就是排序後相鄰元素之間的最大 gap，也正是我們要找的東西。
+
+    // 如果我們用 bucket 來取代個別元素作為比較單位，而且一個 bucket 可以容納多個元素，那比較次數就可以減少。
+    // 因為如果我們還需要比較同一個 bucket 裡面的元素，那其實跟原本排序差不多，沒有變好。
+    // 所以目前的目標是：
+    // 我們希望只比較 bucket 之間，而不要比較 bucket 內部的元素。
+
+    // 我們可以把 bucket size 設得比：
+    // t0 = (max - min) / (n - 1)
+    // 還要小或等於它。
+    // 因為同一個 bucket 裡面的元素 gap 一定不會超過 bucket size，所以最大 gap 就一定會出現在兩個相鄰的非空 bucket 之間。
+    // 因此，只要設定 bucket size b 滿足：
+    // 1 < b <= (max - min) / (n - 1)
+    // 就可以確保至少有一個相鄰 bucket 之間的 gap 會是答案。
+
     int maximumGap(vector<int>& nums) {
 
         // int minElement = INT_MAX/2;
@@ -36,7 +61,7 @@ public:
         int maxElement = *max_element(nums.begin(), nums.end());
 
         int bucketSize = max(1, (maxElement - minElement)/((int)nums.size() - 1));  // bucket size or capacity
-        int bucketNum = (maxElement - minElement) / bucketSize + 1;  // number of buckets
+        int bucketNum = (maxElement - minElement) / bucketSize + 1;  // number of buckets, remember + 1 for celiing
         vector<Bucket> buckets(bucketNum);
 
         for (auto num : nums) {
