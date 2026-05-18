2954544fc4de9fb31ba0a628ec668009b9f8c31b
Li-Wei Yang <68542467+liver121888@users.noreply.github.com>
2026-05-17 20:17:27 -0700

    Time: 0 ms (100%), Space: 12.5 MB (70.94%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,64 +1,71 @@
-// we need to find the peak element in an array
-// we regard the edge as -inf
-// the peak is strictly greater than its neighbors
-// we return any peak
 
-// nums = [1,2,3,1]
-// 3 is peak, return index 2
-
-// nums = [1,2,1,3,5,6,4]
-//  index number 1 or index 5
-
-// O(n), for each position we check the prev and next elements
-// if we find a peak, we return the index
-// if the index is 0 or n - 1, we only compare one side
+// may have duplicates
+// may have multiple peaks
 // [1,2,3,1]
-// 3 > 1, 3 > 2 -> return 
-// l = 0, r = 3, mid = 1
-// 1 2 1, greater to both side
-// we shrink r
 
-// l r mid one index different, and then we return the mid
+// we can O(n) check every spot and it's neighbor
+// TLE
 
-// O(log n)
-// [1,2,peak,1]
-// nums = [1,peak,1,3,5,peak,4]
-// before and after are both sorted
+// peak
+// first apperance of an element larger than both l and r
 
-// [1,peak,1,3,5,peak,4]
-// l = 0, r = 6, mid = 3
+// if we have
+// l = 1
+// r = 1
+// mid = 2
+// mid+1 > n - 1 -> -inf
+// nums[mid+1]
+// see the trend
+// upward trend
+// right side have a peak is guaranteed
+// downawrd trend
+// left side have a peak
 
-// we move one of the pointer until the current mid 
-// l < mid < r
-// mid > l, mid > r
-// r < mid < l
-// mid < l, mid < r
-// we move the pointer until the peak is no longer the peak
+// [1]
+// [1 2]
+
 // [1,2,3,1]
-// l = 1, r = 1, mid = 2
-// l = 2, mid = 3, r = 1 -> we chekc index we return mid
+// 2 3
+// l = 2
+// r = 1
+// mid = 3
+// l = 2
+// r = 3
+// l r = 3
 
+// l r mid
+// 比較 mid + 1 很方便
 
-// [1,peak,1,3,5,peak,4]
-//  1 1 4
-// 1 5 4
+// [1,2,1,3,5,6,4]
+// l 1
+// mid 3
+// r 4
+// midNext 5
+// 5 6 4
+// 6 4
+// l 5
+// r = 6
 
-// stack monotonic 
 
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
 
-        int l = 0, r = nums.size() - 1;
+        int n = nums.size();
+
+        int l = 0;
+        int r = n - 1;
+
         while (l < r) {
-            int mid = l + (r-l)/2;
-            // on a sloping down hill
-            if (nums[mid] > nums[mid + 1])
+            int mid = l + (r - l)/2;
+            int midNext = mid + 1 < n ? nums[mid+1] : INT_MIN/2;
+            if (nums[mid] < midNext) {
+                l = mid + 1;
+            } else {
                 r = mid;
-            else 
-                l = mid+1;
-        }
-        return l;
-        
+            }
+        } 
+
+        return r;
     }
 };
